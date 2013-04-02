9d93bcc7d13e213749ec88a16979b902e1aa98c6
Jiri Olsa <jolsa@redhat.com>
2013-04-02 16:22:47 +0200

    perf list: List formulas counters
    Make default formulas appear in 'perf list' command output
    with [Formula counter].
    
    Signed-off-by: Jiri Olsa <jolsa@redhat.com>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Corey Ashford <cjashfor@linux.vnet.ibm.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Andi Kleen <andi@firstfloor.org>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Ulrich Drepper <drepper@gmail.com>
    Cc: Will Deacon <will.deacon@arm.com>

--- tools/perf/util/parse-events.c
+++ tools/perf/util/parse-events.c
@@ -15,6 +15,7 @@
 #define YY_EXTRA_TYPE int
 #include "parse-events-flex.h"
 #include "pmu.h"
+#include "formula.h"
 
 #define MAX_NAME_LEN 100
 
@@ -1132,6 +1133,8 @@ void print_events(const char *event_glob, bool name_only)
 			    event_symbols_sw, PERF_COUNT_SW_MAX, name_only);
 
 	print_hwcache_events(event_glob, name_only);
+
+	print_formulas();
 
 	if (event_glob != NULL)
 		return;
