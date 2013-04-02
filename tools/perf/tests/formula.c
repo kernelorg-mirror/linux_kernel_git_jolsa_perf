dc68b0546dd6c5557b69f339bc59e242783704a4
Jiri Olsa <jolsa@redhat.com>
2013-04-02 16:14:52 +0200

    perf tests: Add automated tests for formula object
    Adding automated tests to test basic formula
    object definitions and interface.
    
    Signed-off-by: Jiri Olsa <jolsa@redhat.com>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Corey Ashford <cjashfor@linux.vnet.ibm.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Andi Kleen <andi@firstfloor.org>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Ulrich Drepper <drepper@gmail.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: Stephane Eranian <eranian@google.com>

---
+++ tools/perf/tests/formula.c
@@ -0,0 +1,121 @@
+
+#include <linux/kernel.h>
+#include <linux/compiler.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "tests.h"
+#include "formula.h"
+#include "evlist.h"
+#include "evsel.h"
+#include "util.h"
+
+static void file_put(const char *file)
+{
+	unlink(file);
+}
+
+static char *file_get(const char *data, size_t size)
+{
+	static char file[] = "/tmp/perf-tests-formula-XXXXXX";
+	bool failed = false;
+	int fd;
+
+	fd = mkstemp(file);
+	if (fd < 0) {
+		pr_debug("failed to create temp file\n");
+		return NULL;
+	}
+
+	if (size != (size_t) writen(fd, (void *) data, size))
+		failed = true;
+
+	close(fd);
+
+	if (failed)
+		file_put(file);
+
+	return failed ? NULL : file;
+}
+
+static int __test_basics(char *file)
+{
+	struct perf_formula fml;
+	struct perf_formula_set *set;
+	int ret;
+
+	pr_debug("file %s\n", file);
+
+	perf_formula__init(&fml);
+
+	ret = perf_formula__load(&fml, file);
+	TEST_ASSERT_VAL("failed to load formula", !ret);
+
+	set = perf_formula__set(&fml, (char *) "set");
+	TEST_ASSERT_VAL("failed to load set", set);
+	TEST_ASSERT_VAL("wrong set name", !strcmp(set->name, "set"));
+
+	perf_formula__free(&fml);
+	return 0;
+}
+
+static int test_basics(void)
+{
+	int ret;
+	char *file;
+	char data[] =
+"							\n\
+set {							\n\
+	events {					\n\
+		CY = cycles:u				\n\
+		IN = instructions:u			\n\
+	}						\n\
+							\n\
+	t   = -IN / 1000 + 5				\n\
+	cpi = CY / -((t - 5) * 1000)			\n\
+							\n\
+	print cpi					\n\
+}							\n\
+";
+
+	file = file_get(data, sizeof(data));
+	TEST_ASSERT_VAL("failed to get data file", file);
+
+	ret = __test_basics(file);
+
+	file_put(file);
+	return ret;
+}
+
+typedef int (*fn_t)(void);
+
+struct formula_test {
+	const char *desc;
+	fn_t fn;
+} tests[] = {
+	{
+		.desc	= "basics",
+		.fn	= test_basics,
+	},
+	{
+		.desc	= NULL,
+		.fn	= NULL,
+	},
+};
+
+
+int test__formula(void)
+{
+	struct formula_test *t = &tests[0];
+	int ret = 0;
+
+	while (t->desc) {
+		pr_debug("test: %s\n", t->desc);
+
+		if (t->fn())
+			ret = -1;
+
+		t++;
+	}
+
+	return ret;
+}
