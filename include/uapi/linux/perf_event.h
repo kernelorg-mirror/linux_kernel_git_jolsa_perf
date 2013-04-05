4c435ada61337e777ffc6048c78d6af0d01997dc
Jiri Olsa <jolsa@redhat.com>
2013-04-05 17:59:50 +0200

    perf: Add new 'paused' attribute
    Adding new 'paused' perf_event_attr attribute bit. It sets
    the initial event state as paused, so it wont get started
    until it's triggered by toggling mechanism.
    
    Original-patch-by: Frederic Weisbecker <fweisbec@gmail.com>
    Signed-off-by: Jiri Olsa <jolsa@redhat.com>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Corey Ashford <cjashfor@linux.vnet.ibm.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>

--- include/uapi/linux/perf_event.h
+++ include/uapi/linux/perf_event.h
@@ -277,7 +277,9 @@ struct perf_event_attr {
 				exclude_callchain_kernel : 1, /* exclude kernel callchains */
 				exclude_callchain_user   : 1, /* exclude user callchains */
 
-				__reserved_1   : 41;
+				paused         : 1, /* create as paused */
+
+				__reserved_1   : 40;
 
 	union {
 		__u32		wakeup_events;	  /* wakeup every n events */
