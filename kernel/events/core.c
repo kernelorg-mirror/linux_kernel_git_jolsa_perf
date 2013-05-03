7b38ff147220001f613b406d139fc09a5a7caf4e
Jiri Olsa <jolsa@redhat.com>
2013-05-03 09:40:04 +0200

    perf: Add aux flag support to govern auxiliary events
    Adding aux flag to allow delivery of auxiliary events
    eventhough the event itself is disabled.
    
    Signed-off-by: Jiri Olsa <jolsa@redhat.com>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -4416,7 +4416,8 @@ perf_event_aux_ctx(struct perf_event_context *ctx,
 	struct perf_event *event;
 
 	list_for_each_entry_rcu(event, &ctx->event_list, event_entry) {
-		if (event->state < PERF_EVENT_STATE_INACTIVE)
+		if (event->state < PERF_EVENT_STATE_INACTIVE &&
+		    !event->attr.aux)
 			continue;
 		if (!event_filter_match(event))
 			continue;
