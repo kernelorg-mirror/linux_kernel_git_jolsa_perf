4d68fe3e2490e9b5278a03c372991af3a827a1d3
Adrian Hunter <adrian.hunter@intel.com>
2013-06-28 10:50:18 +0300

    perf tools: add pid to struct thread
    Record pid on struct thread.  The member is named 'pid_'
    to avoid confusion with the 'tid' member which was previously
    named 'pid'.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Acked-by: David Ahern <dsahern@gmail.com>

--- tools/perf/util/thread.h
+++ tools/perf/util/thread.h
@@ -12,6 +12,7 @@ struct thread {
 		struct list_head node;
 	};
 	struct map_groups	mg;
+	pid_t			pid_; /* Not all tools update this */
 	pid_t			tid;
 	pid_t			ppid;
 	char			shortname[3];
@@ -24,7 +25,7 @@ struct thread {
 
 struct machine;
 
-struct thread *thread__new(pid_t tid);
+struct thread *thread__new(pid_t pid, pid_t tid);
 void thread__delete(struct thread *self);
 
 int thread__set_comm(struct thread *self, const char *comm);
