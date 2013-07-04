372c17d89240905ae605ceb5baa83b65ad1d871b
Adrian Hunter <adrian.hunter@intel.com>
2013-07-04 15:18:23 +0300

    perf tools: remove references to struct ip_event
    struct ip_event assumes fixed positions for ip, pid
    and tid.  That is no longer true with the addition of
    PERF_SAMPLE_IDENTIFIER.  The information is anyway in
    struct sample, so use that instead.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>

--- tools/perf/util/evsel.c
+++ tools/perf/util/evsel.c
@@ -1222,7 +1222,7 @@ int perf_evsel__parse_sample(struct perf_evsel *evsel, union perf_event *event,
 		return -EFAULT;
 
 	if (type & PERF_SAMPLE_IP) {
-		data->ip = event->ip.ip;
+		data->ip = *array;
 		array++;
 	}
 
@@ -1434,7 +1434,7 @@ int perf_event__synthesize_sample(union perf_event *event, u64 type,
 	array = event->sample.array;
 
 	if (type & PERF_SAMPLE_IP) {
-		event->ip.ip = sample->ip;
+		*array = sample->ip;
 		array++;
 	}
 
