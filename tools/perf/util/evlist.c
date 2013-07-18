21a52a7edaea4f627cbd468e74f2cd1f68a5b418
Adrian Hunter <adrian.hunter@intel.com>
2013-07-18 12:07:32 +0300

    perf tools: move perf_evlist__config() to a new source file
    perf_evlist__config() must be moved to a separate source
    file to avoid Python link errors when adding support for
    PERF_SAMPLE_IDENTIFIER.
    
    It is appropriate to do this because perf_evlist__config()
    is a helper function for event recording.  It is used by
    tools to apply recording options to perf_evlist.  It is
    not used by the Python API.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>

--- tools/perf/util/evlist.c
+++ tools/perf/util/evlist.c
@@ -89,30 +89,6 @@ void perf_evlist__make_sample_types_compatible(struct perf_evlist *evlist)
 	perf_evlist__set_id_pos(evlist);
 }
 
-void perf_evlist__config(struct perf_evlist *evlist,
-			struct perf_record_opts *opts)
-{
-	struct perf_evsel *evsel;
-	/*
-	 * Set the evsel leader links before we configure attributes,
-	 * since some might depend on this info.
-	 */
-	if (opts->group)
-		perf_evlist__set_leader(evlist);
-
-	if (evlist->cpus->map[0] < 0)
-		opts->no_inherit = true;
-
-	list_for_each_entry(evsel, &evlist->entries, node) {
-		perf_evsel__config(evsel, opts);
-
-		if (evlist->nr_entries > 1)
-			perf_evsel__set_sample_id(evsel);
-	}
-
-	perf_evlist__make_sample_types_compatible(evlist);
-}
-
 static void perf_evlist__purge(struct perf_evlist *evlist)
 {
 	struct perf_evsel *pos, *n;
