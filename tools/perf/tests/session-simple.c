dfa338addd21e380c306f70ff9905c7e4e051f74
Jiri Olsa <jolsa@redhat.com>
2013-07-22 22:57:54 +0200

    perf tests: Add session reading test for big endian perf data
    Adding session reading test for big endian perf data
    included as binary data in the test object.
    
    Signed-off-by: Jiri Olsa <jolsa@redhat.com>
    Cc: Corey Ashford <cjashfor@linux.vnet.ibm.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: David Ahern <dsahern@gmail.com>

--- tools/perf/tests/session-simple.c
+++ tools/perf/tests/session-simple.c
@@ -25,6 +25,10 @@ char data_file_v2_le[] = {
 #include "perf.data.v2.le.h"
 };
 
+char data_file_v2_be[] = {
+#include "perf.data.v2.be.h"
+};
+
 static int events_mmap;
 static int events_lost;
 static int events_comm;
@@ -631,5 +635,7 @@ int test__session_simple(void)
 	err = test_generated_data();
 	pr_debug("Testing v2 LE data\n");
 	err |= test_file_data(data_file_v2_le, sizeof(data_file_v2_le));
+	pr_debug("Testing v2 BE data\n");
+	err |= test_file_data(data_file_v2_be, sizeof(data_file_v2_be));
 	return err;
 }
