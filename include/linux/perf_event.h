e17ae2767c91fbd158aad1588003bc4357a2699b
Jiri Olsa <jolsa@redhat.com>
2013-08-15 09:34:14 -0400

    perf: Account toggle masters for toggled event
    Keep track of toggle events within the toggled event.
    It'll be handy for toggle inherit support.
    
    Signed-off-by: Jiri Olsa <jolsa@redhat.com>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Corey Ashford <cjashfor@linux.vnet.ibm.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -423,6 +423,7 @@ struct perf_event {
 	struct perf_event		*toggled_event;
 	enum perf_event_toggle_flag	toggle_flag;
 	int				paused;
+	atomic_t			toggled_cnt;
 #endif /* CONFIG_PERF_EVENTS */
 };
 
