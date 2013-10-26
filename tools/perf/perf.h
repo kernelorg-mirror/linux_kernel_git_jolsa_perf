5cc2b9874fa63bbcdcdd4faf94b9a5dbaaf4a370
Jiri Olsa <jolsa@redhat.com>
2013-10-26 14:34:19 +0200

    perf tools: Add readable output for callchain debug
    Adding people readable output for callchain debug,
    to get following '-v' output:
    
      $ perf record -v -g ls
      callchain: type DWARF
      callchain: stack dump size 4096
      ...
    
    Signed-off-by: Jiri Olsa <jolsa@redhat.com>
    Cc: Corey Ashford <cjashfor@linux.vnet.ibm.com>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Andi Kleen <andi@firstfloor.org>
    Cc: Adrian Hunter <adrian.hunter@intel.com>

--- tools/perf/perf.h
+++ tools/perf/perf.h
@@ -209,7 +209,8 @@ void pthread__unblock_sigwinch(void);
 enum perf_call_graph_mode {
 	CALLCHAIN_NONE,
 	CALLCHAIN_FP,
-	CALLCHAIN_DWARF
+	CALLCHAIN_DWARF,
+	CALLCHAIN_MAX
 };
 
 struct perf_record_opts {
