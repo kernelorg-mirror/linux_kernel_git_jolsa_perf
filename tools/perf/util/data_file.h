70aabae9a9f0a213aaabec38deeeeb5925e2c3f4
Jiri Olsa <jolsa@redhat.com>
2013-10-27 14:40:29 +0100

    perf tools: Move perf_data_file object into data_file file
    Signed-off-by: Jiri Olsa <jolsa@redhat.com>

---
+++ tools/perf/util/data_file.h
@@ -0,0 +1,44 @@
+#ifndef __PERF_DATA_FILE_H
+#define __PERF_DATA_FILE_H
+
+#include <stdbool.h>
+#include "data.h"
+
+struct perf_data_file {
+	const char		*path;
+	int			 fd;
+	bool			 is_pipe;
+	bool			 force;
+	unsigned long		 size;
+	enum perf_data_mode	 mode;
+};
+
+static inline bool perf_data_file__is_read(struct perf_data_file *file)
+{
+	return file->mode == PERF_DATA_MODE_READ;
+}
+
+static inline bool perf_data_file__is_write(struct perf_data_file *file)
+{
+	return file->mode == PERF_DATA_MODE_WRITE;
+}
+
+static inline int perf_data_file__is_pipe(struct perf_data_file *file)
+{
+	return file->is_pipe;
+}
+
+static inline int perf_data_file__fd(struct perf_data_file *file)
+{
+	return file->fd;
+}
+
+static inline unsigned long perf_data_file__size(struct perf_data_file *file)
+{
+	return file->size;
+}
+
+int perf_data_file__open(struct perf_data_file *file);
+void perf_data_file__close(struct perf_data_file *file);
+
+#endif /* __PERF_DATA_FILE_H */
