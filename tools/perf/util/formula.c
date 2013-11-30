a3df8375ff1708350ad92125c2d8a82b8d2164bc
Jiri Olsa <jolsa@redhat.com>
2013-11-30 15:43:15 +0100

    perf formula: Add printf interface
    Adding an interface to:
      - setup list of loaded sets (within the 'struct formula' handler)
         void perf_formula__loaded(struct perf_formula *f,
                                   struct perf_formula_set *set)
    
      - print list of loaded sets
         int perf_formula__print(FILE *file,
                                 struct perf_formula *f,
                                 struct perf_evlist *evlist,
                                 struct perf_formula_value **values);
    
    It's handy (no need to setup extra list) to keep this list within
    the formula object, eventhought its usage is user specific.
    
    Signed-off-by: Jiri Olsa <jolsa@redhat.com>
    Cc: Andi Kleen <andi@firstfloor.org>
    Cc: Corey Ashford <cjashfor@linux.vnet.ibm.com>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Ulrich Drepper <drepper@gmail.com>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Will Deacon <will.deacon@arm.com>

--- tools/perf/util/formula.c
+++ tools/perf/util/formula.c
@@ -17,6 +17,7 @@ void perf_formula__init(struct perf_formula *f)
 {
 	memset(f, 0x0, sizeof(*f));
 	INIT_LIST_HEAD(&f->head_files);
+	INIT_LIST_HEAD(&f->head_sets);
 }
 
 static int scanner_expr(const char *str, void *data)
@@ -445,6 +446,11 @@ static int eval_set(struct perf_formula_set *set,
 			        counter->name);
 			return -1;
 		}
+		if (expr->print && counter->print)
+			fprintf(expr->file,
+				"%'18.8F %-25s\n",
+				expr->result->aggr.result, counter->name);
+
 	}
 
 	return 0;
@@ -787,4 +793,30 @@ struct perf_formula_result *perf_formula__value(struct perf_formula_expr *expr,
 	}
 
 	return result;
+}
+
+void perf_formula__loaded(struct perf_formula *f,
+			  struct perf_formula_set *set)
+{
+	if (list_empty(&set->list_sets))
+		list_add_tail(&set->list_sets, &f->head_sets);
+}
+
+int perf_formula__print(FILE *file,
+			struct perf_formula *f,
+			struct perf_evlist *evlist,
+			struct perf_formula_value **values)
+{
+	struct perf_formula_set *set;
+	struct perf_formula_expr expr = {
+		.evlist = evlist,
+		.values = values,
+		.print = true,
+		.file = file,
+	};
+
+	list_for_each_entry(set, &f->head_sets, list)
+		eval_set(set, &expr);
+
+	return 0;
 }
