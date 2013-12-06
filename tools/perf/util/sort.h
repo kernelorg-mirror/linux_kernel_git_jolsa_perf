2ca7a572b52173466612874e5c0a7a6aee4c4c78
Jiri Olsa <jolsa@redhat.com>
2013-12-06 13:18:34 +0100

    perf tools: Factor sort entries loops
    Factoring sort entries loops so we could transparently
    iterate over hists' specific list of sort entries.
    
    Signed-off-by: Jiri Olsa <jolsa@redhat.com>
    Cc: Corey Ashford <cjashfor@linux.vnet.ibm.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: David Ahern <dsahern@gmail.com>

--- tools/perf/util/sort.h
+++ tools/perf/util/sort.h
@@ -218,4 +218,9 @@ void sort__setup_list(void);
 
 int report_parse_ignore_callees_opt(const struct option *opt, const char *arg, int unset);
 
+bool hists__next_se(struct hists *hists, struct sort_entry **sep);
+
+#define hists__for_each_se(hists, se) \
+	for (se = NULL; hists__next_se(hists, &se);)
+
 #endif	/* __PERF_SORT_H */
