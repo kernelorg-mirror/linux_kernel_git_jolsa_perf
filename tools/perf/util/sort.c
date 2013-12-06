fac1b9bfd378ba4b1078c9d7650a8f014a4d6cba
Jiri Olsa <jolsa@redhat.com>
2013-12-06 13:41:49 +0100

    perf tools: Add local hists sort entry list
    Signed-off-by: Jiri Olsa <jolsa@redhat.com>
    Cc: Corey Ashford <cjashfor@linux.vnet.ibm.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: David Ahern <dsahern@gmail.com>

--- tools/perf/util/sort.c
+++ tools/perf/util/sort.c
@@ -1200,12 +1200,17 @@ void sort__setup_elide(FILE *output)
 bool hists__next_se(struct hists *hists __maybe_unused, struct sort_entry **sep)
 {
 	struct list_head *head_global = &hist_entry__sort_list;
+	struct list_head *head_local  = &hists->hist_entry__sort_list;
 	struct sort_entry *se = *sep;
 
 	if (!se) {
 		se = list_first_entry(head_global, struct sort_entry, list);
 	} else {
 		if (list_is_last(&se->list, head_global)) {
+			se = list_empty(head_local) ? NULL :
+			     list_first_entry(head_local, struct sort_entry,
+					      list);
+		} else if (list_is_last(&se->list, head_local)) {
 			se = NULL;
 		} else {
 			se = list_next_entry(se, list);
@@ -1213,4 +1218,9 @@ bool hists__next_se(struct hists *hists __maybe_unused, struct sort_entry **sep)
 	}
 
 	return se ? (*sep = se) : false;
+}
+
+void hists__sort_entry_add(struct hists *hists, struct sort_entry *se)
+{
+	list_add_tail(&se->list, &hists->hist_entry__sort_list);
 }
