4ecf6c47e619de0680c7b23542bc0fcb184b78f3
Jiri Olsa <jolsa@redhat.com>
2013-12-07 18:31:00 +0100

    perf tools: Add support for tracepoint fields
    Signed-off-by: Jiri Olsa <jolsa@redhat.com>
    Cc: Corey Ashford <cjashfor@linux.vnet.ibm.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: David Ahern <dsahern@gmail.com>

--- tools/perf/builtin-report.c
+++ tools/perf/builtin-report.c
@@ -1044,6 +1044,122 @@ parse_percent_limit(const struct option *opt, const char *str,
 	return 0;
 }
 
+struct field_sort_entry {
+	struct format_field *field;
+	struct sort_entry se;
+};
+
+static bool perf_evsel__is_tracepoint(struct perf_evsel *evsel)
+{
+	return evsel->attr.type == PERF_TYPE_TRACEPOINT;
+}
+
+static int64_t tp_sort_entry__cmp(struct sort_entry *se,
+				  struct hist_entry *left,
+				  struct hist_entry *right)
+{
+	struct field_sort_entry *fse = container_of(se, struct field_sort_entry, se);
+	struct raw_info *raw_left = left->raw_info;
+	struct raw_info *raw_right = right->raw_info;
+
+	return pevent_field_cmp(fse->field,
+				raw_left->data,  raw_left->size,
+				raw_right->data, raw_right->size);
+}
+
+static int tp_sort_entry__snprintf(struct sort_entry *se,
+				   struct hist_entry *he,
+				   char *bf, size_t size,
+				   unsigned int width)
+{
+	struct field_sort_entry *fse = container_of(se, struct field_sort_entry, se);
+	struct raw_info *raw = he->raw_info;
+	struct trace_seq s;
+#define SEQ_SIZE 100
+	char buf[SEQ_SIZE];
+
+	trace_seq_init_buf(&s, buf, SEQ_SIZE);
+	pevent_field_info(&s, fse->field, raw->data, raw->size, false);
+	return scnprintf(bf, size, "%*s", width, buf);
+}
+
+static struct field_sort_entry*
+tp_sort_entry__new(struct format_field *field, int width_idx)
+{
+	struct field_sort_entry* fse = zalloc(sizeof(*fse));
+
+	if (fse) {
+		INIT_LIST_HEAD(&fse->se.list);
+		fse->se.se_header 	= strdup(field->name);
+		fse->se.se_cmp		= tp_sort_entry__cmp;
+		fse->se.se_snprintf	= tp_sort_entry__snprintf;
+		fse->se.se_width_idx	= width_idx;
+		fse->field		= field;
+	}
+
+	return fse;
+}
+
+static int tp_col_width(struct format_field *field)
+{
+	int len = field->size * 2;
+
+	if (field->flags & FIELD_IS_ARRAY)
+		len = 30;
+
+	return len;
+}
+
+static int perf_evsel__add_tp_sort_entries(struct perf_evsel *evsel)
+{
+	struct format_field **fields, **iter;
+	struct hists *hists = &evsel->hists;
+	struct field_sort_entry *fse;
+	int width_idx = HISTC_NR_COLS;
+	int ret = -1;
+
+	fields = iter = pevent_event_fields(evsel->tp_format);
+	if (!fields)
+		return 0;
+
+	while (*iter) {
+		fse = tp_sort_entry__new(*iter, width_idx);
+		if (!fse)
+			goto out;
+
+		if (hists__alloc_col_len(hists, width_idx + 1))
+			goto out;
+
+		hists__set_col_len(hists, width_idx, tp_col_width(*iter));
+		hists__sort_entry_add(hists, &fse->se);
+		width_idx++;
+		iter++;
+	}
+
+	ret = 0;
+
+ out:
+	free(fields);
+	return ret;
+}
+
+static int perf_evlist__add_tp_sort_entries(struct perf_evlist *evlist)
+{
+	struct perf_evsel *evsel;
+	int ret = 0;
+
+	list_for_each_entry(evsel, &evlist->entries, node) {
+		if (!perf_evsel__is_tracepoint(evsel))
+			continue;
+
+		ret = perf_evsel__add_tp_sort_entries(evsel);
+		if (ret)
+			break;
+	}
+
+	return ret;
+}
+
 int cmd_report(int argc, const char **argv, const char *prefix __maybe_unused)
 {
 	struct perf_session *session;
@@ -1310,6 +1426,14 @@ repeat:
 	}
 
 	sort__setup_elide(stdout);
+
+	if (report.show_tp_entries) {
+		ret = perf_evlist__add_tp_sort_entries(session->evlist);
+		if (ret) {
+			pr_err("failed to add tracepoints sort entries\n");
+			goto error;
+		}
+	}
 
 	ret = __cmd_report(&report);
 	if (ret == K_SWITCH_INPUT_DATA) {
