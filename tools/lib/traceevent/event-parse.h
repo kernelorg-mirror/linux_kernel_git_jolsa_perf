2d93519bb0ac18a866c84a1cc2d2f1fa76e4b19f
Jiri Olsa <jolsa@redhat.com>
2013-12-07 17:48:20 +0100

    tools lib traceevent: Factor print_event_fields function
    Factoring print_event_fields function and adding pevent_field_info
    function that is used later in comming patches.
    
    Signed-off-by: Jiri Olsa <jolsa@redhat.com>
    Cc: Corey Ashford <cjashfor@linux.vnet.ibm.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: David Ahern <dsahern@gmail.com>

--- tools/lib/traceevent/event-parse.h
+++ tools/lib/traceevent/event-parse.h
@@ -581,6 +581,10 @@ int pevent_pid_is_registered(struct pevent *pevent, int pid);
 void pevent_print_event(struct pevent *pevent, struct trace_seq *s,
 			struct pevent_record *record, bool use_trace_clock);
 
+void pevent_field_info(struct trace_seq *s,
+		       struct format_field *field,
+		       void *data, int size);
+
 int pevent_parse_header_page(struct pevent *pevent, char *buf, unsigned long size,
 			     int long_size);
 
