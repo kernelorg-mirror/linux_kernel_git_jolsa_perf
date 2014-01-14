3a3a5936c651ce7ea09c9ecd8a9b86a5cf6eda79
Jiri Olsa <jolsa@redhat.com>
2014-01-14 13:47:09 +0100

    tools lib traceevent: Add pevent_field_cmp function
    Adding pevent_field_cmp function to compare 2 fields data,
    the interface is:
    
    int pevent_field_cmp(struct format_field *a_field,
    		     struct format_field *b_field,
    		     void *a_data, int a_size,
    		     void *b_data, int b_size);
    
    Signed-off-by: Jiri Olsa <jolsa@redhat.com>
    Cc: Corey Ashford <cjashfor@linux.vnet.ibm.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: David Ahern <dsahern@gmail.com>

--- tools/lib/traceevent/event-parse.h
+++ tools/lib/traceevent/event-parse.h
@@ -588,6 +588,11 @@ int pevent_pid_is_registered(struct pevent *pevent, int pid);
 void pevent_print_event(struct pevent *pevent, struct trace_seq *s,
 			struct pevent_record *record, bool use_trace_clock);
 
+int pevent_field_cmp(struct format_field *a_field,
+		     struct format_field *b_field,
+		     void *a_data, int a_size,
+		     void *b_data, int b_size);
+
 void pevent_field_info(struct trace_seq *s,
 		       struct format_field *field,
 		       void *data, int size __maybe_unused,
