938f351c9bdd28c31b4b44b1e3137771efdc0574
Jiri Olsa <jolsa@redhat.com>
2014-01-15 17:51:33 +0100

    perf tools: Make perf_format_field__width global
    Making perf_format_field__width global, so it could be
    used from another object in followign patch.
    
    Signed-off-by: Jiri Olsa <jolsa@redhat.com>
    Cc: Corey Ashford <cjashfor@linux.vnet.ibm.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: David Ahern <dsahern@gmail.com>

--- tools/perf/util/report-tp.c
+++ tools/perf/util/report-tp.c
@@ -1,4 +1,5 @@
 #include <traceevent/event-parse.h>
+#include "trace-event.h"
 #include "evlist.h"
 #include "evsel.h"
 #include "sort.h"
@@ -84,7 +85,7 @@ tp_sort_entry__new(struct format_field *field, int width_idx)
 	return field_se;
 }
 
-static int perf_format_field__width(struct format_field *field)
+int perf_format_field__width(struct format_field *field)
 {
 	int len = field->size * 2 + 2 /* '0x' */;
 
