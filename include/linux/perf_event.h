b514e2ac49e29322278ecc9936f9c903d5f6a24a
Jiri Olsa <jolsa@redhat.com>
2014-02-12 11:09:19 -0500

    perf: Add control group event support

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -437,6 +437,14 @@ struct perf_event {
 	int				cgrp_defer_enabled;
 #endif
 
+	union {
+		atomic_t		control_group_id_gen;
+		int			control_group_id;
+	};
+	union {
+		struct list_head	control_list;
+		struct list_head	control_entry;
+	};
 #endif /* CONFIG_PERF_EVENTS */
 };
 
