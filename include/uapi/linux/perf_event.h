8c0d127e11b65f66c6377841ed2dac03cd21742e
Jiri Olsa <jolsa@redhat.com>
2014-02-12 08:27:10 -0500

    perf: Add control_group attribute to perf_event_attr

--- include/uapi/linux/perf_event.h
+++ include/uapi/linux/perf_event.h
@@ -302,7 +302,9 @@ struct perf_event_attr {
 				exclude_callchain_user   : 1, /* exclude user callchains */
 				mmap2          :  1, /* include mmap with inode data     */
 
-				__reserved_1   : 40;
+				group_control  :  1, /* group control */
+
+				__reserved_1   : 39;
 
 	union {
 		__u32		wakeup_events;	  /* wakeup every n events */
