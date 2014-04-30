f8f55fec32a4ddca15026965a61bfdf35655780a
Jiri Olsa <jolsa@kernel.org>
2014-04-30 15:47:27 +0200

    perf tools: Cache dso data file descriptor
    Adding the interface for dso__data_close to allow not
    closing dso data file descriptor:
      void dso__data_close(struct dso *dso, bool cache);
    
    We will keep dsos data file descriptors open until
    its count reaches the half of the current fd open
    limit (RLIMIT_NOFILE). In this case we close file
    descriptor of the first opened dso object.
    
    The report does not even show dso__data_fd function
    for report command processing dwarf unwind stacks.
    
    Output from report over 1.5 GB data with DWARF unwind stacks:
    (TODO fix perf diff)
    
      current code:
       0.10%     perf  perf                       [.] dso__data_fd
      change:
       no record of dso__data_fd
    
    And we got some noticable speed up:
    (perf perf stat -e '{cycles,instructions}:u' ...)
    
      current code:
       108,685,806,301      cycles
       154,562,361,679      instructions              #    1.42  insns per cycle
    
          44.366123152 seconds time elapsed
    
      change:
        86,751,884,833      cycles
       128,225,929,306      instructions              #    1.48  insns per cycle
    
          32.377208550 seconds time elapsed
    
    Cc: Arnaldo Carvalho de Melo <acme@kernel.org>
    Cc: Corey Ashford <cjashfor@linux.vnet.ibm.com>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Jean Pihet <jean.pihet@linaro.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Signed-off-by: Jiri Olsa <jolsa@redhat.com>

--- tools/perf/util/dso.c
+++ tools/perf/util/dso.c
@@ -1,4 +1,6 @@
 #include <asm/bug.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 #include "symbol.h"
 #include "dso.h"
 #include "machine.h"
@@ -180,27 +182,80 @@ static int open_dso(struct dso *dso, struct machine *machine)
 
 static void close_data_fd(struct dso *dso)
 {
-	if (dso->data.fd >= 0) {
-		close(dso->data.fd);
-		dso->data.fd = -1;
-	}
+	close(dso->data.fd);
+	dso->data.fd = -1;
 }
 
 static void close_dso(struct dso *dso)
 {
-	if (dso->data.fd >= 0) {
-		list_del(&dso->data.open_entry);
-		WARN_ONCE(dso__data_open_cnt <= 0,
-			  "DSO data fd counter out of bounds.");
-		dso__data_open_cnt--;
-	}
+	/*
+	 * No need to check for dso->data.fd >= 0 here. We are
+	 * called only from dso__data_close after this check.
+	 */
+	list_del(&dso->data.open_entry);
+	WARN_ONCE(dso__data_open_cnt <= 0,
+		  "DSO data fd counter out of bounds.");
+	dso__data_open_cnt--;
 
 	close_data_fd(dso);
 }
 
-void dso__data_close(struct dso *dso)
+static void close_first_dso(void)
 {
+	struct dso *dso;
+
+	dso = list_first_entry(&dso__data_open, struct dso, data.open_entry);
 	close_dso(dso);
+}
+
+static rlim_t get_fd_limit(void)
+{
+	struct rlimit l;
+	rlim_t limit = 0;
+
+	/* Allow half of the current open fd limit. */
+	if (getrlimit(RLIMIT_NOFILE, &l) == 0) {
+		if (l.rlim_cur == RLIM_INFINITY)
+			limit = l.rlim_cur;
+		else
+			limit = l.rlim_cur / 2;
+	} else {
+		pr_err("failed to get fd limit\n");
+		limit = 1;
+	}
+
+	return limit;
+}
+
+static bool may_cache_fd(void)
+{
+	static rlim_t limit;
+
+	if (!limit)
+		limit = get_fd_limit();
+
+	if (limit == RLIM_INFINITY)
+		return true;
+
+	return limit > (rlim_t) dso__data_open_cnt;
+}
+
+static void data_close(void)
+{
+	bool cache_fd = may_cache_fd();
+
+	if (!cache_fd)
+		close_first_dso();
+}
+
+void dso__data_close(struct dso *dso, bool cache)
+{
+	if (dso->data.fd >= 0) {
+		if (!cache)
+			close_dso(dso);
+		else
+			data_close();
+	}
 }
 
 int dso__data_fd(struct dso *dso, struct machine *machine)
@@ -350,7 +405,7 @@ dso_cache__read(struct dso *dso, struct machine *machine,
 	if (ret <= 0)
 		free(cache);
 
-	dso__data_close(dso);
+	dso__data_close(dso, true);
 	return ret;
 }
 
@@ -557,7 +612,7 @@ void dso__delete(struct dso *dso)
 		dso->long_name_allocated = false;
 	}
 
-	dso__data_close(dso);
+	dso__data_close(dso, false);
 	dso_cache__free(&dso->data.cache);
 	dso__free_a2l(dso);
 	zfree(&dso->symsrc_filename);
