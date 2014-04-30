8cc833fccf7faa5f9204136d01d32228fe048c00
Jiri Olsa <jolsa@kernel.org>
2014-04-30 15:00:59 +0200

    perf tools: Keep global list of opened dso objects
    Adding global list of opened dso objects, so we can
    track opened dso objects and use it for caching dso
    data file descriptors.
    
    Cc: Arnaldo Carvalho de Melo <acme@kernel.org>
    Cc: Corey Ashford <cjashfor@linux.vnet.ibm.com>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Jean Pihet <jean.pihet@linaro.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Signed-off-by: Jiri Olsa <jolsa@redhat.com>

--- tools/perf/util/dso.c
+++ tools/perf/util/dso.c
@@ -136,7 +136,7 @@ int dso__read_binary_type_filename(const struct dso *dso,
 	return ret;
 }
 
-static int open_dso(struct dso *dso, struct machine *machine)
+static int __open_dso(struct dso *dso, struct machine *machine)
 {
 	int fd;
 	char *root_dir = (char *)"";
@@ -159,12 +159,40 @@ static int open_dso(struct dso *dso, struct machine *machine)
 	return fd;
 }
 
-void dso__data_close(struct dso *dso)
+/*
+ * Global list of open DSOs.
+ * Updated by open_dso/dso__data_close.
+ */
+static LIST_HEAD(dso__data_open);
+
+static int open_dso(struct dso *dso, struct machine *machine)
+{
+	int fd = __open_dso(dso, machine);
+
+	if (fd > 0)
+		list_add_tail(&dso->data.open_entry, &dso__data_open);
+	return fd;
+}
+
+static void close_data_fd(struct dso *dso)
 {
 	if (dso->data.fd >= 0) {
 		close(dso->data.fd);
 		dso->data.fd = -1;
 	}
+}
+
+static void close_dso(struct dso *dso)
+{
+	if (dso->data.fd >= 0)
+		list_del(&dso->data.open_entry);
+
+	close_data_fd(dso);
+}
+
+void dso__data_close(struct dso *dso)
+{
+	close_dso(dso);
 }
 
 int dso__data_fd(struct dso *dso, struct machine *machine)
@@ -499,6 +527,7 @@ struct dso *dso__new(const char *name)
 		dso->kernel = DSO_TYPE_USER;
 		dso->needs_swap = DSO_SWAP__UNSET;
 		INIT_LIST_HEAD(&dso->node);
+		INIT_LIST_HEAD(&dso->data.open_entry);
 	}
 
 	return dso;
