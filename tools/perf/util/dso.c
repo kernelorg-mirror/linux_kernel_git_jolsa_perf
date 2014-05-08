fb65b178e570817a1d0c226be8499dce1ffac768
Jiri Olsa <jolsa@kernel.org>
2014-05-08 13:10:38 +0200

    perf tools: Add global count of mmaped dso objects
    Adding global count of mmaped dso objects to keep
    track of how many dso objects are mapped. It will
    be used in following patch.
    
    Cc: Arnaldo Carvalho de Melo <acme@kernel.org>
    Cc: Corey Ashford <cjashfor@linux.vnet.ibm.com>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Jean Pihet <jean.pihet@linaro.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Signed-off-by: Jiri Olsa <jolsa@redhat.com>

--- tools/perf/util/dso.c
+++ tools/perf/util/dso.c
@@ -145,6 +145,7 @@ int dso__read_binary_type_filename(const struct dso *dso,
  */
 static LIST_HEAD(dso__data_open);
 static long dso__data_open_cnt;
+static long dso__data_mmap_cnt;
 
 static void dso__list_add(struct dso *dso)
 {
@@ -158,6 +159,18 @@ static void dso__list_del(struct dso *dso)
 	WARN_ONCE(dso__data_open_cnt <= 0,
 		  "DSO data fd counter out of bounds.");
 	dso__data_open_cnt--;
+}
+
+static void dso__data_mmap_inc(void)
+{
+	dso__data_mmap_cnt++;
+}
+
+static void dso__data_mmap_dec(void)
+{
+	WARN_ONCE(dso__data_mmap_cnt <= 0,
+		  "DSO data mmap counter out of bounds.");
+	dso__data_mmap_cnt--;
 }
 
 static int __open_dso(struct dso *dso, struct machine *machine)
@@ -204,6 +217,7 @@ static void unmap_data_fd(struct dso *dso)
 		pr_debug("dso munmap %s\n", dso->name);
 		dso->data.ptr = NULL;
 		dso->data.all_mmaped = false;
+		dso__data_mmap_dec();
 	}
 }
 
@@ -501,6 +515,8 @@ static int data_mmap(struct dso *dso, u64 offset, ssize_t size)
 		dso->data.cached_read = true;
 		return -1;
 	}
+
+	dso__data_mmap_inc();
 
 	dso->data.ptr       = ptr;
 	dso->data.offset    = offset;
