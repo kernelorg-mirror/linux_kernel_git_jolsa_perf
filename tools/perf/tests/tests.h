437e08668b2c10b8cd681fa9d5fa3f26f273e332
Jiri Olsa <jolsa@kernel.org>
2014-05-12 22:03:07 +0200

    perf tests: Add unmap dso objects on ENOMEM error test
    Testing that perf properly unmap opened dso objects
    and tries to remap dso in case we run out of memory.
    
    Cc: Arnaldo Carvalho de Melo <acme@kernel.org>
    Cc: Corey Ashford <cjashfor@linux.vnet.ibm.com>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Jean Pihet <jean.pihet@linaro.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Signed-off-by: Jiri Olsa <jolsa@redhat.com>

--- tools/perf/tests/tests.h
+++ tools/perf/tests/tests.h
@@ -30,6 +30,7 @@ int test__attr(void);
 int test__dso_data(void);
 int test__dso_data_cache(void);
 int test__dso_data_reopen(void);
+int test__dso_data_remap(void);
 int test__parse_events(void);
 int test__hists_link(void);
 int test__python_use(void);
