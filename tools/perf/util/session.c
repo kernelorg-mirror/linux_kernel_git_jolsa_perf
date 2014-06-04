fd5a859d3fcdedd963f58bb71c355061a95c668b
Jiri Olsa <jolsa@kernel.org>
2014-06-04 17:07:24 +0200

    perf tools: Fix accounting of ordered samples queue

--- tools/perf/util/session.c
+++ tools/perf/util/session.c
@@ -511,6 +511,7 @@ static int flush_sample_queue(struct perf_session *s,
 		os->last_flush = iter->timestamp;
 		list_del(&iter->list);
 		list_add(&iter->list, &os->sample_cache);
+		os->nr_samples--;
 
 		if (show_progress)
 			ui_progress__update(&prog, 1);
@@ -522,8 +523,6 @@ static int flush_sample_queue(struct perf_session *s,
 		os->last_sample =
 			list_entry(head->prev, struct sample_queue, list);
 	}
-
-	os->nr_samples = 0;
 
 	return 0;
 }
