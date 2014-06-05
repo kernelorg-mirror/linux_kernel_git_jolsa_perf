00f9b5639b560c2ba67bdfdf6a5781ad5a5b5d50
Jiri Olsa <jolsa@kernel.org>
2014-06-05 11:00:20 +0200

    perf tools: Add report.queue-size config file option

--- tools/perf/util/session.h
+++ tools/perf/util/session.h
@@ -67,6 +67,8 @@ int perf_session_queue_event(struct perf_session *s, union perf_event *event,
 			     struct perf_tool *tool, struct perf_sample *sample,
 			     u64 file_offset);
 
+void perf_session__sample_queue_size(struct perf_session *session, u64 size);
+
 void perf_tool__fill_defaults(struct perf_tool *tool);
 
 int perf_session__resolve_callchain(struct perf_session *session,
