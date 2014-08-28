8a2ebd7d5ac2ca25a86e7f1deae883962114f0e0
Namhyung Kim <namhyung@kernel.org>
2014-08-28 10:59:33 +0900

    perf tools: Add a test case for timed thread handling
    A test case for verifying live and dead thread tree management during
    time change and new machine__find{,new}_thread_time().
    
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/tests/tests.h
+++ tools/perf/tests/tests.h
@@ -100,6 +100,7 @@ const char *test__clang_subtest_get_desc(int subtest);
 int test__clang_subtest_get_nr(void);
 int test__unit_number__scnprint(int subtest);
 int test__thread_comm(int subtest);
+int test__thread_lookup_time(int subtest);
 
 bool test__bp_signal_is_supported(void);
 
