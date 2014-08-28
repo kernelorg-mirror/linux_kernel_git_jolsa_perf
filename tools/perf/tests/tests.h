23415ecf8ff50201f513a18f3f4abc5cd473b3e0
Namhyung Kim <namhyung@kernel.org>
2014-08-28 10:59:33 +0900

    perf tools: Add a test case for timed map groups handling
    A test case for verifying thread->mg and ->mg_list handling during
    time change and new thread__find_addr_map_by_time() and friends.
    
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/tests/tests.h
+++ tools/perf/tests/tests.h
@@ -100,6 +100,7 @@ int test__clang_subtest_get_nr(void);
 int test__unit_number__scnprint(int subtest);
 int test__thread_comm(int subtest);
 int test__thread_lookup_time(int subtest);
+int test__thread_mg_time(int subtest);
 
 #if defined(__arm__) || defined(__aarch64__)
 #ifdef HAVE_DWARF_UNWIND_SUPPORT
