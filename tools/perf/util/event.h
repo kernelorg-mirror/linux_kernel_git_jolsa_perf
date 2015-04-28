68c2513163bcd329592c40ada0c5de590d5cba31
Jiri Olsa <jolsa@kernel.org>
2015-04-28 23:02:12 +0200

    perf tools: Add stat round event
    Adding stat round event to be stored afer each stat interval round,
    so report tools (report/script) get notified and process interval
    data.
    
    Link: http://lkml.kernel.org/n/tip-nqhzuxrv12ulccx11k7x86mr@git.kernel.org
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/util/event.h
+++ tools/perf/util/event.h
@@ -230,6 +230,7 @@ enum perf_user_event_type { /* above any possible kernel type */
 	PERF_RECORD_CPU_MAP			= 74,
 	PERF_RECORD_STAT_CONFIG			= 75,
 	PERF_RECORD_STAT			= 76,
+	PERF_RECORD_STAT_ROUND			= 77,
 	PERF_RECORD_HEADER_MAX
 };
 
@@ -417,6 +418,11 @@ struct stat_event {
 	};
 };
 
+struct stat_round_event {
+	struct perf_event_header	header;
+	u64				time;
+};
+
 union perf_event {
 	struct perf_event_header	header;
 	struct mmap_event		mmap;
@@ -443,6 +449,7 @@ union perf_event {
 	struct cpu_map_event		cpu_map;
 	struct stat_config_event	stat_config;
 	struct stat_event		stat;
+	struct stat_round_event		stat_round;
 };
 
 void perf_event__print_totals(void);
