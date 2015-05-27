a98693b953b64c940fac9cba391484d14801dc8e
Jiri Olsa <jolsa@kernel.org>
2015-05-27 13:31:38 +0200

    perf stat: Introduce perf_evsel__alloc_stats function
    Move all stat allocation logic related to evsel
    object under single function. This way we can
    use it separately for evsel object out of evlist
    object.
    
    Link: http://lkml.kernel.org/n/tip-hclg66nszn5mx8t9uwrxxw6b@git.kernel.org
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/util/evsel.h
+++ tools/perf/util/evsel.h
@@ -201,6 +201,8 @@ void perf_evsel__reset_stat_priv(struct perf_evsel *evsel);
 int perf_evsel__alloc_stat_priv(struct perf_evsel *evsel);
 void perf_evsel__free_stat_priv(struct perf_evsel *evsel);
 
+int perf_evsel__alloc_stats(struct perf_evsel *evsel, bool alloc_raw);
+
 void perf_evsel__reset_counts(struct perf_evsel *evsel);
 void perf_evsel__free_counts(struct perf_evsel *evsel);
 void perf_evsel__close_fd(struct perf_evsel *evsel, int ncpus, int nthreads);
