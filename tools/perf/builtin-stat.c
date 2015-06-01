720c2723ed3194255ba8daecb7205996c2f31558
Jiri Olsa <jolsa@kernel.org>
2015-06-01 23:36:27 +0200

    perf stat: Add aggr_mode argument to print_shadow_stats function
    As preparation for moving shadow counters code into its own object.
    
    Link: http://lkml.kernel.org/n/tip-kdgryri2kahing584g5is35b@git.kernel.org
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/builtin-stat.c
+++ tools/perf/builtin-stat.c
@@ -1061,7 +1061,8 @@ static void print_ll_cache_misses(FILE *out, int cpu,
 	fprintf(out, " of all LL-cache hits   ");
 }
 
-static void print_shadow_stats(FILE *out, struct perf_evsel *evsel, double avg, int cpu)
+static void print_shadow_stats(FILE *out, struct perf_evsel *evsel,
+			       double avg, int cpu, enum aggr_mode aggr)
 {
 	double total, ratio = 0.0, total2;
 	int ctx = evsel_context(evsel);
@@ -1080,7 +1081,7 @@ static void print_shadow_stats(FILE *out, struct perf_evsel *evsel, double avg, 
 		if (total && avg) {
 			ratio = total / avg;
 			fprintf(out, "\n");
-			if (aggr_mode == AGGR_NONE)
+			if (aggr == AGGR_NONE)
 				fprintf(out, "        ");
 			fprintf(out, "                                                  #   %5.2f  stalled cycles per insn", ratio);
 		}
@@ -1233,7 +1234,7 @@ static void abs_printout(int id, int nr, struct perf_evsel *evsel, double avg)
 	if (csv_output || interval)
 		return;
 
-	print_shadow_stats(output, evsel, avg, cpu);
+	print_shadow_stats(output, evsel, avg, cpu, aggr_mode);
 }
 
 static void print_aggr(char *prefix)
