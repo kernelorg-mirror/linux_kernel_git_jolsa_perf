df09e2b3b03d9624f3cdb1a868ce395984a3efdc
Jiri Olsa <jolsa@kernel.org>
2015-07-02 10:33:19 +0200

    perf tools: Add stat config event synthesize function
    Introduce perf_event__synthesize_stat_config function to
    sythesize 'struct perf_stat_config'.
    
    Storing stat config in form of tag-value pairs in a believe
    it'll sort out future version issues.
    
    Link: http://lkml.kernel.org/n/tip-zof1adf5flwp38xdzxzn4p2z@git.kernel.org
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/tests/tests.h
+++ tools/perf/tests/tests.h
@@ -67,6 +67,7 @@ int test__insn_x86(void);
 int test_session_topology(void);
 int test__thread_map_synthesize(void);
 int test__cpu_map_synthesize(void);
+int test__synthesize_stat_config(void);
 
 #if defined(__x86_64__) || defined(__i386__) || defined(__arm__) || defined(__aarch64__)
 #ifdef HAVE_DWARF_UNWIND_SUPPORT
