e8a1ee8e81d8d3cbc9627752918830a4b626adc1
Jiri Olsa <jolsa@kernel.org>
2015-07-08 18:17:52 +0200

    perf tools: Add stat event synthesize function
    Introduce perf_event__synthesize_stat function to
    synthesize 'struct stat_event'.
    
    Link: http://lkml.kernel.org/n/tip-0jr9x4vwk0nxbs2ueclj7jfy@git.kernel.org
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/tests/tests.h
+++ tools/perf/tests/tests.h
@@ -70,6 +70,7 @@ int test_session_topology(void);
 int test__thread_map_synthesize(void);
 int test__cpu_map_synthesize(void);
 int test__synthesize_stat_config(void);
+int test__synthesize_stat(void);
 
 #if defined(__arm__) || defined(__aarch64__)
 #ifdef HAVE_DWARF_UNWIND_SUPPORT
