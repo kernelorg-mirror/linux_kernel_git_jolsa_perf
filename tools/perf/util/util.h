5fded1acb4703d340b3a4b6f4d6c7d377defaa6f
Jiri Olsa <jolsa@kernel.org>
2015-10-01 10:00:07 +0200

    perf tools: Do not read symbols/data from device files
    With mem sampling we could get data source within mapped
    device file. Processing such sample would block during
    report phase on trying to read the device file.
    
    Chacking for device files and skip the processing if
    it's detected.
    
    Link: http://lkml.kernel.org/n/tip-rdj48ax089rhsdzpy31r48fw@git.kernel.org
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/util/util.h
+++ tools/perf/util/util.h
@@ -357,4 +357,6 @@ int fetch_kernel_version(unsigned int *puint,
 #define KVER_FMT	"%d.%d.%d"
 #define KVER_PARAM(x)	KVER_VERSION(x), KVER_PATCHLEVEL(x), KVER_SUBLEVEL(x)
 
+bool is_regular_file(const char *file);
+
 #endif /* GIT_COMPAT_UTIL_H */
