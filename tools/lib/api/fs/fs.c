b01fc56387d061e7f37a8103bc9593879f05354e
Jiri Olsa <jolsa@kernel.org>
2015-11-23 11:15:40 +0100

    tools lib api fs: Move filename__read_str into api/fs/fs.c
    We already moved similar functions in here,
    also it'll be useful for sysfs__read_str
    addition in following patch.
    
    Loosing one pr_warning as we don't have global
    api debug support yet. It does not seem like
    a big deal thought.
    
    Link: http://lkml.kernel.org/n/tip-azxqdrdsw6fpfpk9ycmyp5eb@git.kernel.org
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/lib/api/fs/fs.c
+++ tools/lib/api/fs/fs.c
@@ -300,6 +300,51 @@ int filename__read_ull(const char *filename, unsigned long long *value)
 	return err;
 }
 
+int filename__read_str(const char *filename, char **buf, size_t *sizep)
+{
+	size_t size = 0, alloc_size = 0;
+	void *bf = NULL, *nbf;
+	int fd, n, err = 0;
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return -errno;
+
+	do {
+		if (size == alloc_size) {
+			alloc_size += BUFSIZ;
+			nbf = realloc(bf, alloc_size);
+			if (!nbf) {
+				err = -ENOMEM;
+				break;
+			}
+
+			bf = nbf;
+		}
+
+		n = read(fd, bf + size, alloc_size - size);
+		if (n < 0) {
+			if (size) {
+				err = 0;
+			} else
+				err = -errno;
+
+			break;
+		}
+
+		size += n;
+	} while (n > 0);
+
+	if (!err) {
+		*sizep = size;
+		*buf   = bf;
+	} else
+		free(bf);
+
+	close(fd);
+	return err;
+}
+
 int sysfs__read_ull(const char *entry, unsigned long long *value)
 {
 	char path[PATH_MAX];
