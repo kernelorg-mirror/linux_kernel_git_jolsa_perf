294019e46c236c5455aa684359522183a12aee30
Jiri Olsa <jolsa@kernel.org>
2015-12-31 11:40:25 +0100

    perf tools: Properly release format fields
    Adding support to properly relase format output/sort fields.
    
    Link: http://lkml.kernel.org/n/tip-4gs7pj4kgbvgs38vyj7m9z0i@git.kernel.org
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/ui/hist.c
+++ tools/perf/ui/hist.c
@@ -595,6 +595,12 @@ next:
 	}
 }
 
+static void fmt_free(struct perf_hpp_fmt *fmt)
+{
+	if (fmt->free)
+		fmt->free(fmt);
+}
+
 void perf_hpp__reset_output_field(void)
 {
 	struct perf_hpp_fmt *fmt, *tmp;
@@ -603,12 +609,14 @@ void perf_hpp__reset_output_field(void)
 	perf_hpp__for_each_format_safe(fmt, tmp) {
 		list_del_init(&fmt->list);
 		list_del_init(&fmt->sort_list);
+		fmt_free(fmt);
 	}
 
 	/* reset sort keys */
 	perf_hpp__for_each_sort_list_safe(fmt, tmp) {
 		list_del_init(&fmt->list);
 		list_del_init(&fmt->sort_list);
+		fmt_free(fmt);
 	}
 }
 
