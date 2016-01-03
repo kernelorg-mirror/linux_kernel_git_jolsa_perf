d4d54d0be01f6b2223d4954cdd199b4feebdf30e
Jiri Olsa <jolsa@kernel.org>
2016-01-03 15:03:02 +0100

    perf tools: Introduce struct perf_hpp_list
    Gather output and sort lists under struct perf_hpp_list,
    so we could have multiple instancies of sort/output format
    entries.
    
    Replacing current perf_hpp__list and perf_hpp__sort_list
    lists with single perf_hpp_list instance.
    
    Link: http://lkml.kernel.org/n/tip-bdn1hamlwp2529thajbrh2w7@git.kernel.org
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/util/hist.h
+++ tools/perf/util/hist.h
@@ -226,20 +226,24 @@ struct perf_hpp_fmt {
 	int idx;
 };
 
-extern struct list_head perf_hpp__list;
-extern struct list_head perf_hpp__sort_list;
+struct perf_hpp_list {
+	struct list_head list;
+	struct list_head sort_list;
+};
+
+extern struct perf_hpp_list perf_hpp_list;
 
 #define perf_hpp__for_each_format(format) \
-	list_for_each_entry(format, &perf_hpp__list, list)
+	list_for_each_entry(format, &perf_hpp_list.list, list)
 
 #define perf_hpp__for_each_format_safe(format, tmp)	\
-	list_for_each_entry_safe(format, tmp, &perf_hpp__list, list)
+	list_for_each_entry_safe(format, tmp, &perf_hpp_list.list, list)
 
 #define perf_hpp__for_each_sort_list(format) \
-	list_for_each_entry(format, &perf_hpp__sort_list, sort_list)
+	list_for_each_entry(format, &perf_hpp_list.sort_list, sort_list)
 
 #define perf_hpp__for_each_sort_list_safe(format, tmp)	\
-	list_for_each_entry_safe(format, tmp, &perf_hpp__sort_list, sort_list)
+	list_for_each_entry_safe(format, tmp, &perf_hpp_list.sort_list, sort_list)
 
 extern struct perf_hpp_fmt perf_hpp__format[];
 
