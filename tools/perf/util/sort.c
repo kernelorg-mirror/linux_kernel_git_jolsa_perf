4834a9c75910b7bfd2af4257f3557f91dac66650
Jiri Olsa <jolsa@kernel.org>
2016-01-03 18:46:37 +0100

    perf tools: Add struct perf_hpp_list argument to helper functions
    Adding struct perf_hpp_list argument to following helper functions:
      void perf_hpp__setup_output_field(struct perf_hpp_list *list);
      void perf_hpp__reset_output_field(struct perf_hpp_list *list);
      void perf_hpp__append_sort_keys(struct perf_hpp_list *list);
    
    so they could be used on hists's hpp_list.
    
    Link: http://lkml.kernel.org/n/tip-t5g9q42thptl6qaorydp2v16@git.kernel.org
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/util/sort.c
+++ tools/perf/util/sort.c
@@ -2631,9 +2631,9 @@ int setup_sorting(struct perf_evlist *evlist)
 		return err;
 
 	/* copy sort keys to output fields */
-	perf_hpp__setup_output_field();
+	perf_hpp__setup_output_field(&perf_hpp_list);
 	/* and then copy output fields to sort keys */
-	perf_hpp__append_sort_keys();
+	perf_hpp__append_sort_keys(&perf_hpp_list);
 
 	return 0;
 }
@@ -2649,5 +2649,5 @@ void reset_output_field(void)
 	sort_order = NULL;
 
 	reset_dimensions();
-	perf_hpp__reset_output_field();
+	perf_hpp__reset_output_field(&perf_hpp_list);
 }
