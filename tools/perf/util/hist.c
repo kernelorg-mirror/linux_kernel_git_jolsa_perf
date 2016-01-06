ac8df41a5639a34d34ca5520d7df007b7674a5ec
Jiri Olsa <jolsa@kernel.org>
2016-01-06 16:30:04 +0100

    perf tools: Fix HISTC_MEM_DCACHELINE width setting
    Set correct with for unresolved addr.
    
    Link: http://lkml.kernel.org/n/tip-llh940fx5l1em3au9sse2lss@git.kernel.org
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/util/hist.c
+++ tools/perf/util/hist.c
@@ -131,6 +131,7 @@ void hists__calc_col_len(struct hists *hists, struct hist_entry *h)
 			symlen = unresolved_col_width + 4 + 2;
 			hists__new_col_len(hists, HISTC_MEM_DADDR_SYMBOL,
 					   symlen);
+			hists__new_col_len(hists, HISTC_MEM_DCACHELINE, 25);
 		}
 
 		if (h->mem_info->iaddr.sym) {
