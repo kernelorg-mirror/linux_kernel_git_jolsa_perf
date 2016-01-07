23303af96e0d3769e8c9770a6cec513db458b8a3
Jiri Olsa <jolsa@kernel.org>
2016-01-07 22:49:53 +0100

    perf tools: Add c2c hitm related sort keys
    Link: http://lkml.kernel.org/n/tip-a2apq6nb9kdj0boyfy76cmsk@git.kernel.org
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/util/hist.c
+++ tools/perf/util/hist.c
@@ -173,7 +173,10 @@ void hists__calc_col_len(struct hists *hists, struct hist_entry *h)
 	hists__new_col_len(hists, HISTC_C2C_DADDR, 25);
 	hists__new_col_len(hists, HISTC_C2C_IADDR, 25);
 	hists__new_col_len(hists, HISTC_C2C_DCACHELINE_OFFSET, 3);
-	hists__new_col_len(hists, HISTC_C2C_STATS_NR, 4);
+	hists__new_col_len(hists, HISTC_C2C_STATS_NR, 7);
+	hists__new_col_len(hists, HISTC_C2C_STATS_TOT_HITM, 7);
+	hists__new_col_len(hists, HISTC_C2C_STATS_RMT_HITM, 7);
+	hists__new_col_len(hists, HISTC_C2C_STATS_LCL_HITM, 7);
 
 	if (h->srcline)
 		hists__new_col_len(hists, HISTC_SRCLINE, strlen(h->srcline));
