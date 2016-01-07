afcd8f916d133585f7be243faafa58b44c33eb64
Jiri Olsa <jolsa@kernel.org>
2016-01-07 23:57:00 +0100

    perf tools: Add c2c llc load related sort keys
    Link: http://lkml.kernel.org/n/tip-ewn1dxu39zcg9rul6ikbrb1v@git.kernel.org
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/util/hist.c
+++ tools/perf/util/hist.c
@@ -183,6 +183,8 @@ void hists__calc_col_len(struct hists *hists, struct hist_entry *h)
 	hists__new_col_len(hists, HISTC_C2C_STATS_LD_FBHIT, 7);
 	hists__new_col_len(hists, HISTC_C2C_STATS_LD_L1HIT, 7);
 	hists__new_col_len(hists, HISTC_C2C_STATS_LD_L2HIT, 7);
+	hists__new_col_len(hists, HISTC_C2C_STATS_LD_LLCHIT, 7);
+	hists__new_col_len(hists, HISTC_C2C_STATS_LD_RMTHIT, 7);
 
 	if (h->srcline)
 		hists__new_col_len(hists, HISTC_SRCLINE, strlen(h->srcline));
