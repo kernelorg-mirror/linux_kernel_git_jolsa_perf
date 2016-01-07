358c7c5917aa4021b159970a5cb67e2cd95b20d3
Jiri Olsa <jolsa@kernel.org>
2016-01-07 21:28:50 +0100

    perf tools: Introduce hist_browser__init
    Factoring out the hist_browser initialization code,
    so it could be used from other parts in following
    patches.
    
    Link: http://lkml.kernel.org/n/tip-wpnz9geiwesk0stkiumwc904@git.kernel.org
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/ui/browsers/hists.c
+++ tools/perf/ui/browsers/hists.c
@@ -1492,25 +1492,30 @@ static int hist_browser__dump(struct hist_browser *browser)
 	return 0;
 }
 
+static void hist_browser__init(struct hist_browser *browser,
+			       struct hists *hists)
+{
+	struct perf_hpp_fmt *fmt;
+
+	browser->hists			= hists;
+	browser->b.refresh		= hist_browser__refresh;
+	browser->b.refresh_dimensions 	= hist_browser__refresh_dimensions;
+	browser->b.seek			= ui_browser__hists_seek;
+	browser->b.use_navkeypressed 	= true;
+	browser->show_headers		= symbol_conf.show_hist_headers;
+
+	perf_hpp__for_each_format(fmt) {
+		perf_hpp__reset_width(fmt, hists);
+		++browser->b.columns;
+	}
+}
+
 static struct hist_browser* hist_browser__new(struct hists *hists)
 {
 	struct hist_browser *browser = zalloc(sizeof(*browser));
 
-	if (browser) {
-		struct perf_hpp_fmt *fmt;
-
-		browser->hists = hists;
-		browser->b.refresh = hist_browser__refresh;
-		browser->b.refresh_dimensions = hist_browser__refresh_dimensions;
-		browser->b.seek = ui_browser__hists_seek;
-		browser->b.use_navkeypressed = true;
-		browser->show_headers = symbol_conf.show_hist_headers;
-
-		perf_hpp__for_each_format(fmt) {
-			perf_hpp__reset_width(fmt, hists);
-			++browser->b.columns;
-		}
-	}
+	if (browser)
+		hist_browser__init(browser, hists);
 
 	return browser;
 }
