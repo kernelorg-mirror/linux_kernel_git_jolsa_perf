c611d7a265cc93ae1870038dcc31eb5afbf974a2
Jiri Olsa <jolsa@kernel.org>
2016-01-08 14:41:39 +0100

    perf tools: Add c2c cacheline hitm/store percent related sort keys
    Link: http://lkml.kernel.org/n/tip-99thdx1oi1i4vhevh03z6oit@git.kernel.org
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/util/hist.c
+++ tools/perf/util/hist.c
@@ -188,6 +188,10 @@ void hists__calc_col_len(struct hists *hists, struct hist_entry *h)
 	hists__new_col_len(hists, HISTC_C2C_STATS_TOT_RECS, 7);
 	hists__new_col_len(hists, HISTC_C2C_STATS_PERCENT_HITM, 7);
 	hists__new_col_len(hists, HISTC_C2C_STATS_PERCENT_LDMISS, 7);
+	hists__new_col_len(hists, HISTC_C2C_STATS_PERCENT_RMT_HITM, 7);
+	hists__new_col_len(hists, HISTC_C2C_STATS_PERCENT_LCL_HITM, 7);
+	hists__new_col_len(hists, HISTC_C2C_STATS_PERCENT_ST_L1HIT, 7);
+	hists__new_col_len(hists, HISTC_C2C_STATS_PERCENT_ST_L1MISS, 7);
 
 	if (h->srcline)
 		hists__new_col_len(hists, HISTC_SRCLINE, strlen(h->srcline));
