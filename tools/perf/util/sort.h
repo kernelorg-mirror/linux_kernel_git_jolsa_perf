8398e9a3d07429e0fb3e19b604be547919f96258
Jiri Olsa <jolsa@kernel.org>
2016-04-28 16:51:20 +0200

    perf tools: Introduce hist_entry_ops

--- tools/perf/util/sort.h
+++ tools/perf/util/sort.h
@@ -74,6 +74,11 @@ struct hist_entry_diff {
 	};
 };
 
+struct hist_entry_ops {
+	void*	(*new)(size_t size);
+	void	(*free)(void *);
+};
+
 /**
  * struct hist_entry - histogram entry
  *
@@ -132,6 +137,7 @@ struct hist_entry {
 	void			*trace_output;
 	struct perf_hpp_list	*hpp_list;
 	struct hist_entry	*parent_he;
+	struct hist_entry_ops	*ops;
 	union {
 		/* this is for hierarchical entry structure */
 		struct {
