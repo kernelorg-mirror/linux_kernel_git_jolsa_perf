504c6f84b3d9f3de7f1b0eb5e9e2868100c6065d
Jiri Olsa <jolsa@kernel.org>
2016-04-29 14:25:31 +0200

    perf c2c report: Fallback to standard dimensions

--- tools/perf/builtin-c2c.c
+++ tools/perf/builtin-c2c.c
@@ -197,8 +197,10 @@ static int c2c_hists__init_output(struct c2c_hists *hists, char *name)
 {
 	struct c2c_fmt *c2c_fmt = get_format(name);
 
-	if (!c2c_fmt)
-		return -1;
+	if (!c2c_fmt) {
+		reset_dimensions();
+		return output_field_add(&hists->list, name);
+	}
 
 	perf_hpp_list__column_register(&hists->list, &c2c_fmt->fmt);
 	return 0;
@@ -208,8 +210,10 @@ static int c2c_hists__init_sort(struct c2c_hists *hists, char *name)
 {
 	struct c2c_fmt *c2c_fmt = get_format(name);
 
-	if (!c2c_fmt)
-		return -1;
+	if (!c2c_fmt) {
+		reset_dimensions();
+		return sort_dimension__add(&hists->list, name, NULL, 0);
+	}
 
 	perf_hpp_list__register_sort_field(&hists->list, &c2c_fmt->fmt);
 	return 0;
