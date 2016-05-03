74e3915d7d742518723ffb282f9f75e6f0ab9ff1
Jiri Olsa <jolsa@kernel.org>
2016-05-03 20:19:21 +0200

    perf c2c report: Add daddr dimension key

--- tools/perf/builtin-c2c.c
+++ tools/perf/builtin-c2c.c
@@ -293,9 +293,41 @@ static int offset_entry(struct perf_hpp_fmt *fmt, struct perf_hpp *hpp,
 	return snprintf(hpp->buf, hpp->size, "0x%-*" PRIx64, width, addr);
 }
 
+static int64_t
+daddr_cmp(struct perf_hpp_fmt *fmt __maybe_unused,
+	  struct hist_entry *left, struct hist_entry *right)
+{
+	u64 l, r;
+
+	if (!left->mem_info)  return -1;
+	if (!right->mem_info) return 1;
+
+	/* al_addr does all the right addr - start + offset calculations */
+	l = left->mem_info->daddr.addr;
+	r = right->mem_info->daddr.addr;
+
+	if (l > r) return -1;
+	if (l < r) return 1;
+
+	return 0;
+}
+
+static int daddr_entry(struct perf_hpp_fmt *fmt, struct perf_hpp *hpp,
+		       struct hist_entry *he)
+{
+	uint64_t addr = 0;
+	int width = c2c_width(fmt, hpp, he->hists);
+
+	if (he->mem_info)
+		addr = he->mem_info->daddr.al_addr;
+
+	return snprintf(hpp->buf, hpp->size, "0x%-*" PRIx64, width, addr);
+}
+
 enum {
 	DIM_DCACHELINE,
 	DIM_OFFSET,
+	DIM_DADDR,
 };
 
 #define HEADER(__h)				\
@@ -317,11 +349,20 @@ static struct c2c_dimension dim_offset = {
 	.id		= DIM_OFFSET,
 };
 
+static struct c2c_dimension dim_daddr = {
+	HEADER("data address"),
+	.name		= "daddr",
+	.cmp		= daddr_cmp,
+	.entry		= daddr_entry,
+	.id		= DIM_DADDR,
+};
+
 #undef HEADER
 
 static struct c2c_dimension *dimensions[] = {
 	&dim_dcacheline,
 	&dim_offset,
+	&dim_daddr,
 	NULL,
 };
 
@@ -333,6 +374,9 @@ static void set_dimension(struct c2c_dimension *dim)
 		break;
 	case DIM_OFFSET:
 		dim->width = 5;
+		break;
+	case DIM_DADDR:
+		dim->width = 20;
 		break;
 	default:
 		pr_err("internal dimension error\n");
