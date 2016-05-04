3b1ec8a12ea6b0b9376297ae13da84379157d10f
Jiri Olsa <jolsa@kernel.org>
2016-05-04 10:50:09 +0200

    perf c2c report: Add hitm percent sort key

--- tools/perf/builtin-c2c.c
+++ tools/perf/builtin-c2c.c
@@ -813,6 +813,60 @@ tot_loads_cmp(struct perf_hpp_fmt *fmt __maybe_unused,
 	return tot_recs_left - tot_recs_right;
 }
 
+static double percent_hitm(struct c2c_hist_entry *c2c_he)
+{
+	struct c2c_hists *hists;
+	struct c2c_stats *stats;
+	struct c2c_stats *total;
+	int tot, st;
+	double p;
+
+	hists = container_of(c2c_he->he.hists, struct c2c_hists, hists);
+	stats = &c2c_he->stats;
+	total = &hists->stats;
+
+	st  = stats->lcl_hitm + stats->rmt_hitm;
+	tot = total->lcl_hitm + total->rmt_hitm;
+
+	p = tot ? (double) st / tot : 0;
+
+	return 100 * p;
+}
+
+static int
+percent_hitm_entry(struct perf_hpp_fmt *fmt, struct perf_hpp *hpp,
+		   struct hist_entry *he)
+{
+	struct c2c_hist_entry *c2c_he;
+	int width = c2c_width(fmt, hpp, he->hists);
+	char buf[10];
+	double per;
+
+	c2c_he = container_of(he, struct c2c_hist_entry, he);
+	per = percent_hitm(c2c_he);
+
+	snprintf(buf, 10, "%.2F%%", per);
+	return snprintf(hpp->buf, hpp->size, "%*s", width, buf);
+}
+
+static int64_t
+percent_hitm_cmp(struct perf_hpp_fmt *fmt __maybe_unused,
+		 struct hist_entry *left, struct hist_entry *right)
+{
+	struct c2c_hist_entry *c2c_left;
+	struct c2c_hist_entry *c2c_right;
+	double per_left;
+	double per_right;
+
+	c2c_left  = container_of(left, struct c2c_hist_entry, he);
+	c2c_right = container_of(right, struct c2c_hist_entry, he);
+
+	per_left  = percent_hitm(c2c_left);
+	per_right = percent_hitm(c2c_right);
+
+	return per_left - per_right;
+}
+
 enum {
 	DIM_DCACHELINE,
 	DIM_OFFSET,
@@ -832,6 +886,7 @@ enum {
 	DIM_LD_LLC_MISS,
 	DIM_TOT_RECS,
 	DIM_TOT_LOADS,
+	DIM_PERCENT_HITM,
 };
 
 /* HEADER_* macros are for main browser */
@@ -1077,6 +1132,14 @@ static struct c2c_dimension dim_tot_loads = {
 	.id		= DIM_TOT_LOADS,
 };
 
+static struct c2c_dimension dim_percent_hitm = {
+	HEADER_0("%hitm"),
+	.name		= "percent_hitm",
+	.cmp		= percent_hitm_cmp,
+	.entry		= percent_hitm_entry,
+	.id		= DIM_PERCENT_HITM,
+};
+
 #undef HEADER_0
 #undef HEADER_1
 #undef HEADER_SPAN
@@ -1111,6 +1174,7 @@ static struct c2c_dimension *dimensions[] = {
 	&dim_ld_llcmiss,
 	&dim_tot_recs,
 	&dim_tot_loads,
+	&dim_percent_hitm,
 	NULL,
 };
 
@@ -1135,6 +1199,7 @@ static void set_dimension(struct c2c_dimension *dim)
 	case DIM_LD_LLC_MISS:
 	case DIM_TOT_RECS:
 	case DIM_TOT_LOADS:
+	case DIM_PERCENT_HITM:
 		dim->width = 7;
 		break;
 	case DIM_TOT_HITM:
