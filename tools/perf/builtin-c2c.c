5e546c40cfb7930d08a7d4ae3253d4ba34f8f1b7
Jiri Olsa <jolsa@kernel.org>
2016-05-25 08:50:10 +0200

    perf c2c report: Add tid sort key

--- tools/perf/builtin-c2c.c
+++ tools/perf/builtin-c2c.c
@@ -1529,6 +1529,12 @@ static struct c2c_dimension dim_pid = {
 	.width		= 7,
 };
 
+static struct c2c_dimension dim_tid = {
+	HEADER_CL_0("Tid"),
+	.name		= "tid",
+	.se		= &sort_thread,
+};
+
 #undef HEADER_0
 #undef HEADER_1
 #undef HEADER_SPAN
@@ -1571,6 +1577,7 @@ static struct c2c_dimension *dimensions[] = {
 	&dim_dram_lcl,
 	&dim_dram_rmt,
 	&dim_pid,
+	&dim_tid,
 	NULL,
 };
 
