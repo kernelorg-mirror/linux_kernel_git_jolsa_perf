fe285210705972f76de82aa8742e04a39301775a
Jiri Olsa <jolsa@kernel.org>
2016-05-25 08:50:10 +0200

    perf c2c report: Add tid sort key
    Adding tid dimension key wrapper.
    
    It is to be displayed in the single cacheline output:
    
      tid
    
    It's a wrapper for global sort_thread sort entry with
    c2c specific header.
    
    Link: http://lkml.kernel.org/n/tip-fr0socae5skzvz5qbkl85prn@git.kernel.org
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/builtin-c2c.c
+++ tools/perf/builtin-c2c.c
@@ -1426,6 +1426,12 @@ static struct c2c_dimension dim_pid = {
 	.width		= 7,
 };
 
+static struct c2c_dimension dim_tid = {
+	.header		= HEADER_LOW("Tid"),
+	.name		= "tid",
+	.se		= &sort_thread,
+};
+
 #undef HEADER_LOW
 #undef HEADER_BOTH
 #undef HEADER_SPAN
@@ -1462,6 +1468,7 @@ static struct c2c_dimension *dimensions[] = {
 	&dim_dram_lcl,
 	&dim_dram_rmt,
 	&dim_pid,
+	&dim_tid,
 	NULL,
 };
 
