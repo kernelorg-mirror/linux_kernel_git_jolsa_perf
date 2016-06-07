420dba865edac4d5c18f6038c6ecabf302f0104d
Jiri Olsa <jolsa@kernel.org>
2016-06-07 09:01:32 +0200

    perf tools: Introduce watch command

---
+++ tools/perf/builtin-watch.c
@@ -0,0 +1,9 @@
+#include <linux/compiler.h>
+#include "builtin.h"
+#include "perf.h"
+
+int cmd_watch(int argc __maybe_unused, const char **argv __maybe_unused,
+	      const char *prefix __maybe_unused)
+{
+	return 0;
+}
