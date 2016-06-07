fe8701140f01915877bcdb021f32c4ce58ad767f
Jiri Olsa <jolsa@kernel.org>
2016-06-07 19:43:48 +0200

    perf c2c report: Add dsymbol dimension key

--- tools/perf/builtin-c2c.c
+++ tools/perf/builtin-c2c.c
@@ -475,6 +475,11 @@ static struct c2c_dimension dim_iaddr = {
 	.width		= 20,
 };
 
+static struct c2c_dimension dim_dsymbol = {
+	.name		= "dsymbol",
+	.se		= &sort_mem_daddr_sym,
+};
+
 #undef HEADER_0
 #undef HEADER_1
 #undef HEADER_SPAN
@@ -489,6 +494,7 @@ static struct c2c_dimension *dimensions[] = {
 	&dim_dcacheline,
 	&dim_offset,
 	&dim_iaddr,
+	&dim_dsymbol,
 	NULL,
 };
 
