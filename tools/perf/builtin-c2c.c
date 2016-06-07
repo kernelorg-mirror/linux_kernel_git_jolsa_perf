1128e15699ac0688f9d48f2f3ed5218a6847c3b3
Jiri Olsa <jolsa@kernel.org>
2016-06-08 01:00:05 +0200

    perf c2c report: Introduce symbol_conf.display_raw_addr

--- tools/perf/builtin-c2c.c
+++ tools/perf/builtin-c2c.c
@@ -2658,6 +2658,8 @@ static int perf_c2c__report(int argc, const char **argv)
 
 	setup_browser(false);
 
+	symbol_conf.display_raw_addr = true;
+
 	if (!input_name || !strlen(input_name))
 		input_name = "perf.data";
 
