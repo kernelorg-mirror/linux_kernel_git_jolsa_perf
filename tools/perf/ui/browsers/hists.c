4290ec52a130d6a4a77673fafd8294243425d6e1
Jiri Olsa <jolsa@kernel.org>
2016-06-10 22:43:26 +0200

    perf tools: Make several display functions global

--- tools/perf/ui/browsers/hists.c
+++ tools/perf/ui/browsers/hists.c
@@ -1078,7 +1078,7 @@ struct hpp_arg {
 	bool current_entry;
 };
 
-static int __hpp__slsmg_color_printf(struct perf_hpp *hpp, const char *fmt, ...)
+int __hpp__slsmg_color_printf(struct perf_hpp *hpp, const char *fmt, ...)
 {
 	struct hpp_arg *arg = hpp->ptr;
 	int ret, len;
