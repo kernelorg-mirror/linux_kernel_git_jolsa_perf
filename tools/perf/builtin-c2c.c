bf5f9787b857dd352430402f47da29d910e76428
Jiri Olsa <jolsa@kernel.org>
2016-06-23 23:05:52 +0200

    perf c2c report: Add cpu cnt sort key

--- tools/perf/builtin-c2c.c
+++ tools/perf/builtin-c2c.c
@@ -1278,8 +1278,8 @@ node_entry(struct perf_hpp_fmt *fmt __maybe_unused, struct perf_hpp *hpp,
 			break;
 		case 1:
 		{
-			int num = bitmap_weight(c2c_he->cpuset, c2c.cpus_cnt);
 			struct c2c_stats *stats = &c2c_he->node_stats[node];
+			int num = bitmap_weight(c2c_he->cpuset, c2c.cpus_cnt);
 
 			ret = scnprintf(hpp->buf, hpp->size, "%2d{%2d ", node, num);
 			advance_hpp(hpp, ret);
@@ -1363,6 +1363,20 @@ stddev_entry(struct perf_hpp_fmt *fmt __maybe_unused, struct perf_hpp *hpp,
 	return snprintf(hpp->buf, hpp->size, "%*s", width, buf);
 }
 
+static int
+cpucnt_entry(struct perf_hpp_fmt *fmt __maybe_unused, struct perf_hpp *hpp,
+	     struct hist_entry *he)
+{
+	struct c2c_hist_entry *c2c_he;
+	int width = c2c_width(fmt, hpp, he->hists);
+	char buf[10];
+
+	c2c_he = container_of(he, struct c2c_hist_entry, he);
+
+	snprintf(buf, 10, "%d", bitmap_weight(c2c_he->cpuset, c2c.cpus_cnt));
+	return snprintf(hpp->buf, hpp->size, "%*s", width, buf);
+}
+
 /* HEADER_* macros are for main browser */
 
 #define HEADER_0(__h)	\
@@ -1399,9 +1413,12 @@ stddev_entry(struct perf_hpp_fmt *fmt __maybe_unused, struct perf_hpp *hpp,
 		.text = __h,	\
 	}
 
-#define HEADER_CL_1(__h)	\
+#define HEADER_CL_1(__h0, __h1)	\
 	.header[0] = {		\
-		.text = __h,	\
+		.text = __h0,	\
+	},			\
+	.header[1] = {		\
+		.text = __h1,	\
 	}
 
 #define HEADER_CL_SPAN(__h0, __h1, __s)	\
@@ -1738,6 +1755,14 @@ static struct c2c_dimension dim_stddev = {
 	.width		= 8,
 };
 
+static struct c2c_dimension dim_cpucnt = {
+	HEADER_CL_1("cpu", "cnt"),
+	.name		= "cpucnt",
+	.cmp		= empty_cmp,
+	.entry		= cpucnt_entry,
+	.width		= 8,
+};
+
 #undef HEADER_0
 #undef HEADER_1
 #undef HEADER_SPAN
@@ -1787,6 +1812,7 @@ static struct c2c_dimension *dimensions[] = {
 	&dim_median,
 	&dim_mean,
 	&dim_stddev,
+	&dim_cpucnt,
 	NULL,
 };
 
