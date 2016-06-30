e888a90f9675ed5664023edf8e71e018e269893e
Jiri Olsa <jolsa@kernel.org>
2016-06-30 09:09:01 +0200

    perf script python: Fix string vs byte array resolving
    Recognize byte array from string and pass proper object
    to user's script.
    
    Link: http://lkml.kernel.org/n/tip-22f4vhhz5uytegkggy1on8u3@git.kernel.org
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/util/python.c
+++ tools/perf/util/python.c
@@ -299,7 +299,7 @@ static int is_printable_array(char *p, unsigned int len)
 {
 	unsigned int i;
 
-	for (i = 0; i < len && p[i]; i++)
+	for (i = 0; i < len; i++)
 		if (!isprint(p[i]) && !isspace(p[i]))
 		    return 0;
 	return 1;
