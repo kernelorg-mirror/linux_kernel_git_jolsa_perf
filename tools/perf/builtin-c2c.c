c88ebd413f8c18722290bb4a0165bdd7930d36d5
Jiri Olsa <jolsa@kernel.org>
2016-07-10 15:47:40 +0200

    perf c2c report: Add src line sort key

--- tools/perf/builtin-c2c.c
+++ tools/perf/builtin-c2c.c
@@ -41,6 +41,8 @@ struct perf_c2c {
 	int			 cpus_cnt;
 	int			*cpu2node;
 	int			 node_info;
+
+	bool			 show_src;
 };
 
 static struct perf_c2c c2c;
@@ -1768,6 +1770,11 @@ static struct c2c_dimension dim_cpucnt = {
 	.width		= 8,
 };
 
+static struct c2c_dimension dim_srcline = {
+	.name		= "cl_srcline",
+	.se		= &sort_srcline,
+};
+
 #undef HEADER_0
 #undef HEADER_1
 #undef HEADER_SPAN
@@ -1818,6 +1825,7 @@ static struct c2c_dimension *dimensions[] = {
 	&dim_mean,
 	&dim_stddev,
 	&dim_cpucnt,
+	&dim_srcline,
 	NULL,
 };
 
@@ -2025,6 +2033,9 @@ static int c2c_hists__reinit(struct c2c_hists *c2c_hists,
 
 static int filter_cb(struct hist_entry *he __maybe_unused)
 {
+	if (c2c.show_src && !he->srcline)
+		he->srcline = hist_entry__get_srcline(he);
+
 	return 0;
 }
 
