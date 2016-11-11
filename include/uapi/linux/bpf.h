6a2784bb60843bbd28fa5e69932fbbf81ef633d6
Jiri Olsa <jolsa@kernel.org>
2016-11-11 21:42:59 +0100

    dwarf_unwind: Add bpf unwind prog type

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -105,6 +105,7 @@ enum bpf_prog_type {
 	BPF_PROG_TYPE_LWT_IN,
 	BPF_PROG_TYPE_LWT_OUT,
 	BPF_PROG_TYPE_LWT_XMIT,
+	BPF_PROG_TYPE_UNWIND,
 };
 
 enum bpf_attach_type {
@@ -476,7 +477,8 @@ union bpf_attr {
 	FN(set_hash_invalid),		\
 	FN(get_numa_node_id),		\
 	FN(skb_change_head),		\
-	FN(xdp_adjust_head),
+	FN(xdp_adjust_head),		\
+	FN(unwind),
 
 /* integer value in 'imm' field of BPF_CALL instruction selects which helper
  * function eBPF program intends to call
