9b42224e82fa6905805e3fa53382d574652a703e
Jiri Olsa <jolsa@kernel.org>
2017-01-13 14:48:46 +0100

    perf tools: Create separate mmap for dummy tracking event
    When indexed data file support is enabled, a dummy tracking event will
    be used to track metadata (like task, comm and mmap events) for a
    session and actual samples will be recorded in separate (intermediate)
    files and then merged (with index table).
    
    Provide separate mmap to the dummy tracking event.  The size is fixed
    to 128KiB (+ 1 page) as the event rate will be lower than samples.  I
    originally wanted to use a single mmap for this but cross-cpu sharing
    is prohibited so it's per-cpu (or per-task) like normal mmaps.
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/builtin-record.c
+++ tools/perf/builtin-record.c
@@ -500,6 +500,8 @@ static int record__mmap_read_evlist(struct record *rec, struct perf_evlist *evli
 
 	for (i = 0; i < evlist->nr_mmaps; i++) {
 		struct perf_mmap *map = &maps[i];
+		struct perf_mmap *track_map =  evlist->track_mmap ?
+					      &evlist->track_mmap[i] : NULL;
 
 		if (map->base) {
 			if (perf_mmap__push(map, overwrite, rec, record__pushfn) != 0) {
@@ -512,6 +514,13 @@ static int record__mmap_read_evlist(struct record *rec, struct perf_evlist *evli
 		    record__auxtrace_mmap_read(rec, map) != 0) {
 			rc = -1;
 			goto out;
+		}
+
+		if (track_map && track_map->base) {
+			if (perf_mmap__push(track_map, overwrite, rec, record__pushfn) != 0) {
+				rc = -1;
+				goto out;
+			}
 		}
 	}
 
