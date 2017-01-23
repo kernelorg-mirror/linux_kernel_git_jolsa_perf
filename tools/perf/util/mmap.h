0de497d34301a53813612a42bda51c6f79f355ee
Jiri Olsa <jolsa@kernel.org>
2017-01-23 23:13:18 +0100

    perf tools: Add struct perf_mmap arg into record__write
    The map argument will hold the pointer for the file
    to write the data to.
    
    Link: http://lkml.kernel.org/n/tip-hnoi427zudjnm86awuejhnnt@git.kernel.org
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/util/mmap.h
+++ tools/perf/util/mmap.h
@@ -89,8 +89,8 @@ static inline void perf_mmap__write_tail(struct perf_mmap *md, u64 tail)
 union perf_event *perf_mmap__read_forward(struct perf_mmap *map);
 union perf_event *perf_mmap__read_backward(struct perf_mmap *map);
 
-int perf_mmap__push(struct perf_mmap *md, bool backward,
-		    void *to, int push(void *to, void *buf, size_t size));
+int perf_mmap__push(struct perf_mmap *md, bool overwrite, void *to,
+		    int push(struct perf_mmap *map, void *to, void *buf, size_t size));
 
 size_t perf_mmap__mmap_len(struct perf_mmap *map);
 
