a4eea115518d5ad2030f7fd9e04943b86a23e814
Jiri Olsa <jolsa@kernel.org>
2017-01-23 22:25:41 +0100

    perf tools: Add struct perf_data_file

--- tools/perf/builtin-kvm.c
+++ tools/perf/builtin-kvm.c
@@ -1052,9 +1052,9 @@ static int read_events(struct perf_kvm_stat *kvm)
 		.ordered_events		= true,
 	};
 	struct perf_data file = {
-		.path = kvm->file_name,
-		.mode = PERF_DATA_MODE_READ,
-		.force = kvm->force,
+		.file.path = kvm->file_name,
+		.mode      = PERF_DATA_MODE_READ,
+		.force     = kvm->force,
 	};
 
 	kvm->tool = eops;
