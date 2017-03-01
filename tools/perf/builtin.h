8e8fd7ba314106bd4dd6fad7df84163995ff26ec
Jiri Olsa <jolsa@kernel.org>
2017-03-01 15:46:28 +0100

    perf tools: Add rdt subcommand

--- tools/perf/builtin.h
+++ tools/perf/builtin.h
@@ -42,6 +42,7 @@ int cmd_inject(int argc, const char **argv);
 int cmd_mem(int argc, const char **argv);
 int cmd_data(int argc, const char **argv);
 int cmd_ftrace(int argc, const char **argv);
+int cmd_rdt(int argc, const char **argv);
 
 int find_scripts(char **scripts_array, char **scripts_path_array);
 #endif
