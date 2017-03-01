8baf4044e456b169ba51ecd84e8ab291c0a3779f
Jiri Olsa <jolsa@kernel.org>
2017-03-01 16:31:56 +0100

    perf tools: Move perf_evlist::workload into perf_workload
    Aso it can be used in commands that do not care about
    touching struct evlist.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

---
+++ tools/perf/util/workload.h
@@ -0,0 +1,15 @@
+#ifndef __PERF_WORKLOAD_H
+#define __PERF_WORKLOAD_H
+
+struct perf_workload {
+	int	cork_fd;
+	pid_t	pid;
+};
+
+int perf_workload__prepare(struct perf_workload *workload,
+			   const char *argv[], bool pipe_output,
+			   void (*exec_error)(int signo, siginfo_t *info,
+			   void *ucontext));
+int perf_workload__start(struct perf_workload *workload);
+
+#endif /* __PERF_WORKLOAD_H */
