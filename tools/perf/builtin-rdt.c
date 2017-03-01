61e66d7a9adb3586cc500a98ea8c12b08eb1f73c
Jiri Olsa <jolsa@kernel.org>
2017-03-01 16:43:13 +0100

    perf tools rdt: Add group option
    Adding group option to specify the current operation
    deals with specific group.
    
    Adding default action to run a workload under specific
    group via following command:
    
      # perf rdt -g krava perf bench sched pipe
    
    This will run perf bench under group 'krava' and
    waits for it to finish.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/builtin-rdt.c
+++ tools/perf/builtin-rdt.c
@@ -3,9 +3,42 @@
 #include "perf.h"
 #include "builtin.h"
 #include "debug.h"
+#include "workload.h"
 #include <subcmd/parse-options.h>
 #include <api/fs/fs.h>
 #include "rdt.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+static int move_task(int pid, const char *group)
+{
+	char path[PATH_MAX], buf[20];
+	struct stat st;
+	FILE *file;
+	int cnt;
+
+	scnprintf(path, PATH_MAX, "%s/%s/tasks", resctrlfs__mount(), group);
+
+	if (stat(path, &st)) {
+		pr_err("failed: group not found\n");
+		return -1;
+	}
+
+	file = fopen(path, "w");
+	if (!file) {
+		pr_err("failed: open file '%s'\n", path);
+		return -1;
+	}
+
+	cnt = scnprintf(buf, 20, "%d\n", pid);
+
+	if (1 != fwrite(buf, cnt, 1, file))
+		pr_err("failed: to write to '%s'\n", path);
+
+	fclose(file);
+	return 0;
+}
 
 static int setup_resctrl(void)
 {
@@ -46,8 +79,11 @@ int cmd_rdt(int argc, const char **argv)
 		"perf rdt [<options>] -- <command> [<options>]",
 		NULL
 	};
+	static const char *group;
 	const struct option rdt_options[] = {
 	OPT_INCR('v', "verbose", &verbose, "be more verbose"),
+	OPT_STRING('g', "group", &group, "group",
+		   "group to attach workload to"),
 	OPT_END()
 	};
 
@@ -59,7 +95,21 @@ int cmd_rdt(int argc, const char **argv)
 	if (!argc)
 		usage_with_options(rdt_usage, rdt_options);
 
-	if (!strncmp(argv[0], "dump", 4)) {
+	if (group) {
+		struct perf_workload workload;
+		int status;
+
+		if (perf_workload__prepare(&workload, argv, false, NULL))
+			return -1;
+
+		if (move_task(workload.pid, group))
+			return -1;
+
+		if (!perf_workload__start(&workload))
+			return -1;
+
+		wait(&status);
+	} else if (!strncmp(argv[0], "dump", 4)) {
 		return perf_rdt__dump(argc, argv);
 	} else {
                 usage_with_options(rdt_usage, rdt_options);
