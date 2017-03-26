5fac37b148bcf5a07306fe03e57deff460d631a6
Jiri Olsa <jolsa@kernel.org>
2017-03-26 13:09:16 +0200

    perf tools rdt: Add dump subcommand
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/builtin-rdt.c
+++ tools/perf/builtin-rdt.c
@@ -1,7 +1,43 @@
+#include <stdio.h>
+#include <linux/compiler.h>
 #include "perf.h"
 #include "builtin.h"
 #include "debug.h"
 #include <subcmd/parse-options.h>
+#include <api/fs/fs.h>
+#include "rdt.h"
+
+static int setup_resctrl(void)
+{
+	if (!resctrlfs__mount()) {
+		pr_err("failed: no resctrl fs mount found\n");
+		return -1;
+	}
+
+	return 0;
+}
+
+static int perf_rdt__dump(int argc, const char **argv)
+{
+	bool close_file = false;
+	FILE *file_out = stdout;
+	int ret;
+
+	setup_pager();
+
+	if (argc == 2) {
+		file_out = fopen(argv[1], "w+");
+		if (!file_out)
+			return -1;
+		close_file = true;
+	}
+
+	ret = rdt_dump(file_out);
+
+	if (close_file)
+		fclose(file_out);
+	return ret;
+}
 
 int cmd_rdt(int argc, const char **argv)
 {
@@ -15,10 +51,19 @@ int cmd_rdt(int argc, const char **argv)
 	OPT_END()
 	};
 
+	if (setup_resctrl())
+		return -1;
+
 	argc = parse_options(argc, argv, rdt_options, rdt_usage,
 			     PARSE_OPT_STOP_AT_NON_OPTION);
 	if (!argc)
 		usage_with_options(rdt_usage, rdt_options);
+
+	if (!strncmp(argv[0], "dump", 4)) {
+		return perf_rdt__dump(argc, argv);
+	} else {
+                usage_with_options(rdt_usage, rdt_options);
+	}
 
 	return 0;
 }
