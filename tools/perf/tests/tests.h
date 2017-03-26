c71c4a34e6e71da148b15e48c93053f9178d86f0
Jiri Olsa <jolsa@kernel.org>
2017-03-26 11:05:12 +0200

    perf tests rdt: Add rdt parser test
    Adding test for RDT JSON parser.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/tests/tests.h
+++ tools/perf/tests/tests.h
@@ -100,6 +100,7 @@ const char *test__clang_subtest_get_desc(int subtest);
 int test__clang_subtest_get_nr(void);
 int test__unit_number__scnprint(int subtest);
 int test__cpu_map__read(int subtest);
+int test__rdt_parser(int subtest);
 
 bool test__bp_signal_is_supported(void);
 
