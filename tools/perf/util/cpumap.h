7f17c9cc88d59372632243db3ded17432eeed710
Jiri Olsa <jolsa@kernel.org>
2017-03-27 10:29:07 +0200

    perf tools: Rename cpu_map__read to cpu_map__read_list
    Kernel makes a difference between cpus and cpus files.
    The first display pure cpu bitmap while the latter
    display easily readable cpu list, which we currently
    support in perf.
    
    Renaming cpu_map__read to cpu_map__read_list so we can
    add support for pure cpu bitmap reading in following
    patch.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/util/cpumap.h
+++ tools/perf/util/cpumap.h
@@ -18,7 +18,7 @@ struct cpu_map *cpu_map__new(const char *cpu_list);
 struct cpu_map *cpu_map__empty_new(int nr);
 struct cpu_map *cpu_map__dummy_new(void);
 struct cpu_map *cpu_map__new_data(struct cpu_map_data *data);
-struct cpu_map *cpu_map__read(FILE *file);
+struct cpu_map *cpu_map__read_list(FILE *file);
 size_t cpu_map__snprint(struct cpu_map *map, char *buf, size_t size);
 size_t cpu_map__snprint_mask(struct cpu_map *map, char *buf, size_t size);
 size_t cpu_map__fprintf(struct cpu_map *map, FILE *fp);
