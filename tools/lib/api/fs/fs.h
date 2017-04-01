399c69f9f11843aa6186da54613c579b2bbe07da
Jiri Olsa <jolsa@kernel.org>
2017-04-01 15:59:10 +0200

    perf tools: Add filename__read_xll function
    Some kernel files do not follow the 0x prefix in front
    of the hex number. Adding filename__read_xll function
    to be able to read such files.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/lib/api/fs/fs.h
+++ tools/lib/api/fs/fs.h
@@ -30,6 +30,7 @@ FS(resctrlfs)
 
 int filename__read_int(const char *filename, int *value);
 int filename__read_ull(const char *filename, unsigned long long *value);
+int filename__read_xll(const char *filename, unsigned long long *value);
 int filename__read_str(const char *filename, char **buf, size_t *sizep);
 
 int procfs__read_str(const char *entry, char **buf, size_t *sizep);
