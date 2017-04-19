5fe4cbcaacc4ffa0370997f8f212e7f142d8b98e
Jiri Olsa <jolsa@kernel.org>
2017-04-19 18:38:35 +0200

    x86/intel_rdt: Add rdt group id file

--- arch/x86/kernel/cpu/intel_rdt_rdtgroup.c
+++ arch/x86/kernel/cpu/intel_rdt_rdtgroup.c
@@ -463,6 +463,22 @@ static int rdtgroup_tasks_show(struct kernfs_open_file *of,
 	return ret;
 }
 
+static int rdtgroup_id_show(struct kernfs_open_file *of,
+			    struct seq_file *s, void *v)
+{
+	struct rdtgroup *rdtgrp;
+	int ret = 0;
+
+	rdtgrp = rdtgroup_kn_lock_live(of->kn);
+	if (rdtgrp)
+		seq_printf(s, "%d\n", rdtgrp->closid);
+	else
+		ret = -ENOENT;
+	rdtgroup_kn_unlock(of->kn);
+
+	return ret;
+}
+
 /* Files in each rdtgroup */
 static struct rftype rdtgroup_base_files[] = {
 	{
@@ -485,6 +501,12 @@ static struct rftype rdtgroup_base_files[] = {
 		.kf_ops		= &rdtgroup_kf_single_ops,
 		.write		= rdtgroup_schemata_write,
 		.seq_show	= rdtgroup_schemata_show,
+	},
+	{
+		.name		= "id",
+		.mode		= 0444,
+		.kf_ops		= &rdtgroup_kf_single_ops,
+		.seq_show	= rdtgroup_id_show,
 	},
 };
 
