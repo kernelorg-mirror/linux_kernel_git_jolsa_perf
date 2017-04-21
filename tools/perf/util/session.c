90ac839c2872d9c70d628bcc1e5c1de236ebdde9
Jiri Olsa <jolsa@kernel.org>
2017-04-21 16:53:32 +0200

    perf record: Add --closid option
    Adding --closid option that enables PERF_SAMPLE_CLOSID
    sample type for recorded events.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/util/session.c
+++ tools/perf/util/session.c
@@ -1123,6 +1123,9 @@ static void dump_sample(struct perf_evsel *evsel, union perf_event *event,
 
 	if (sample_type & PERF_SAMPLE_READ)
 		sample_read__printf(sample, evsel->attr.read_format);
+
+	if (sample_type & PERF_SAMPLE_CLOSID)
+		printf(" ... closid: %" PRIu32 "\n", sample->closid);
 }
 
 static struct machine *machines__find_for_cpumode(struct machines *machines,
