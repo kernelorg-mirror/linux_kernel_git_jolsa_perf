94e09fcd829d56712c1f6148636bbee8978055a4
Jiri Olsa <jolsa@kernel.org>
2017-04-25 15:43:52 +0200

    perf tools rdt: Add base structures

---
+++ tools/perf/util/rdt.h
@@ -0,0 +1,48 @@
+#ifndef __PERF_RDT_H
+#define __PERF_RDT_H
+
+#include <linux/list.h>
+#include <linux/types.h>
+#include <cpumap.h>
+
+enum {
+	RDT_RESOURCE_L3,
+	RDT_RESOURCE_L3DATA,
+	RDT_RESOURCE_L3CODE,
+	RDT_RESOURCE_L2,
+	RDT_NUM_RESOURCES,
+};
+
+struct rdt_cbm {
+	u64	id;
+	u64	val;
+};
+
+struct rdt_schemata {
+	bool		 enabled;
+	int		 cnt;
+	struct rdt_cbm	*cbm;
+};
+
+struct rdt_resource {
+	const char	*name;
+	bool		 enabled;
+	u64		 cbm_mask;
+	u64		 min_cbm_bits;
+	u64		 num_closids;
+};
+
+struct rdt_group {
+	int			 id;
+	const char		*name;
+	struct rdt_schemata	 schemata[RDT_NUM_RESOURCES];
+	struct cpu_map		*cpus;
+	struct list_head	 list;
+};
+
+struct rdt_data {
+	struct list_head	 groups;
+	struct rdt_resource	 resource[RDT_NUM_RESOURCES];
+};
+
+#endif /* __PERF_RDT_H */
