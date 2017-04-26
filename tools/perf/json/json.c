266faca91b9bd88277fb7a4e2f29f1c18813791a
Jiri Olsa <jolsa@kernel.org>
2017-04-26 14:25:36 +0200

    perf tools: Add parse_json_map function
    Adding parse_json_map function to allow parsing from
    already  opened map/string.  The current interface
    allows only passing the file name.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/json/json.c
+++ tools/perf/json/json.c
@@ -76,40 +76,57 @@ static void unmapfile(char *map, size_t size)
 	munmap(map, roundup(size, ps));
 }
 
-/*
- * Parse json file using jsmn. Return array of tokens,
- * and mapped file. Caller needs to free array.
- */
-jsmntok_t *parse_json(const char *fn, char **map, size_t *size, int *len)
+static jsmntok_t *__parse_json(const char *fn, char *map, size_t size, int *len)
 {
 	jsmn_parser parser;
 	jsmntok_t *tokens;
 	jsmnerr_t res;
 	unsigned sz;
 
-	*map = mapfile(fn, size);
-	if (!*map)
-		return NULL;
 	/* Heuristic */
-	sz = *size * 16;
+	sz = size * 16;
 	tokens = malloc(sz);
 	if (!tokens)
-		goto error;
+		return NULL;
+
 	jsmn_init(&parser);
-	res = jsmn_parse(&parser, *map, *size, tokens,
+	res = jsmn_parse(&parser, map, size, tokens,
 			 sz / sizeof(jsmntok_t));
 	if (res != JSMN_SUCCESS) {
-		pr_err("%s: json error %s\n", fn, jsmn_strerror(res));
+		pr_err("%s: json error %s\n", fn ?: "none", jsmn_strerror(res));
 		goto error_free;
 	}
 	if (len)
 		*len = parser.toknext;
 	return tokens;
+
 error_free:
 	free(tokens);
-error:
-	unmapfile(*map, *size);
 	return NULL;
+}
+
+/*
+ * Parse json file using jsmn. Return array of tokens,
+ * and mapped file. Caller needs to free array.
+ */
+jsmntok_t *parse_json(const char *fn, char **map, size_t *size, int *len)
+{
+	jsmntok_t *tokens;
+
+	*map = mapfile(fn, size);
+	if (!*map)
+		return NULL;
+
+	tokens = __parse_json(fn, *map, *size, len);
+	if (!tokens)
+		unmapfile(*map, *size);
+
+	return tokens;
+}
+
+jsmntok_t *parse_json_map(char *map, size_t size, int *len)
+{
+	return __parse_json(NULL, map, size, len);
 }
 
 void free_json(char *map, size_t size, jsmntok_t *tokens)
