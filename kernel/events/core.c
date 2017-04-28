d90effc64438f656f37de90aa2788e31fd10d88b
Jiri Olsa <jolsa@kernel.org>
2017-04-28 07:44:54 -0400

    perf/x86/core: Add rdt closid sample type
    Adding new PERF_SAMPLE_CLOSID sample type to attach closid
    value to the perf sample.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    
    Conflicts:
    	arch/x86/kernel/cpu/intel_rdt.c

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -1568,6 +1568,9 @@ static void __perf_event_header_size(struct perf_event *event, u64 sample_type)
 	if (sample_type & PERF_SAMPLE_TRANSACTION)
 		size += sizeof(data->txn);
 
+	if (sample_type & PERF_SAMPLE_CLOSID)
+		size += sizeof(data->closid);
+
 	event->header_size = size;
 }
 
@@ -5948,6 +5951,9 @@ void perf_output_sample(struct perf_output_handle *handle,
 		}
 	}
 
+	if (sample_type & PERF_SAMPLE_CLOSID)
+		perf_output_put(handle, data->closid);
+
 	if (!event->attr.watermark) {
 		int wakeup_events = event->attr.wakeup_events;
 
@@ -5961,6 +5967,11 @@ void perf_output_sample(struct perf_output_handle *handle,
 			}
 		}
 	}
+}
+
+__weak u64 arch_rdt_get_closid(void)
+{
+	return (u64) -1;
 }
 
 void perf_prepare_sample(struct perf_event_header *header,
@@ -6081,6 +6092,9 @@ void perf_prepare_sample(struct perf_event_header *header,
 
 		header->size += size;
 	}
+
+	if (sample_type & PERF_SAMPLE_CLOSID)
+		data->closid = arch_rdt_get_closid();
 }
 
 static void __always_inline
