8243900c8c6e5d83484dcdd7670b481cad967730
Jiri Olsa <jolsa@kernel.org>
2017-05-03 01:43:36 +0200

    perf report: Add rdt_group sort entry
    Addinf --sort rdt_group support to sort the hist entries
    based on the RDT group id via the samples closid values.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/builtin-report.c
+++ tools/perf/builtin-report.c
@@ -162,6 +162,7 @@ static int process_sample_event(struct perf_tool *tool,
 	struct hist_entry_iter iter = {
 		.evsel 			= evsel,
 		.sample 		= sample,
+		.session		= rep->session,
 		.hide_unresolved 	= symbol_conf.hide_unresolved,
 		.add_entry_cb 		= hist_iter__report_callback,
 	};
