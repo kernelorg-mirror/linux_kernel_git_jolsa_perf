73e856183e4dc6452db611807b45e8f69e09fe81
Jiri Olsa <jolsa@kernel.org>
2017-05-18 20:06:50 +0200

    perf tools: Add thread_map__new_file function
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/util/thread_map.h
+++ tools/perf/util/thread_map.h
@@ -24,6 +24,7 @@ struct thread_map *thread_map__new_by_tid(pid_t tid);
 struct thread_map *thread_map__new_by_uid(uid_t uid);
 struct thread_map *thread_map__new(pid_t pid, pid_t tid, uid_t uid);
 struct thread_map *thread_map__new_event(struct thread_map_event *event);
+struct thread_map *thread_map__new_file(const char *filename);
 
 struct thread_map *thread_map__get(struct thread_map *map);
 void thread_map__put(struct thread_map *map);
