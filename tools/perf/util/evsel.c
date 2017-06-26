509f3a556c220393687fbee8fd3e1fc90df42e2a
Jiri Olsa <jolsa@kernel.org>
2017-06-27 00:14:31 +0200

    perf tools: Fix -n option
    The kernel fails to add a non sampling event event
    with having precise_ip set.
    
    Make sure the precise_ip is 0 when using -n option
    to create non sampling event in record.
    
    Link: http://lkml.kernel.org/n/tip-l513fzc1houvu7tl1bo5y65y@git.kernel.org
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/util/evsel.c
+++ tools/perf/util/evsel.c
@@ -890,8 +890,10 @@ void perf_evsel__config(struct perf_evsel *evsel, struct record_opts *opts,
 		attr->sample_period = 0;
 	}
 
-	if (opts->no_samples)
+	if (opts->no_samples) {
 		attr->sample_freq = 0;
+		attr->precise_ip = 0;
+	}
 
 	if (opts->inherit_stat)
 		attr->inherit_stat = 1;
