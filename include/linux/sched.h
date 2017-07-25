f4952243f555ff26cc27cae14f60855b8aa1c35b
Jiri Olsa <jolsa@kernel.org>
2017-07-25 11:36:35 +0200

    perf: Add PR_TASK_PERF_EVENTS_DATA_USER prctl

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -923,6 +923,8 @@ struct task_struct {
 	struct perf_event_context	*perf_event_ctxp[perf_nr_task_contexts];
 	struct mutex			perf_event_mutex;
 	struct list_head		perf_event_list;
+	char				*perf_data_user_ptr;
+	unsigned int			 perf_data_user_size;
 #endif
 #ifdef CONFIG_DEBUG_PREEMPT
 	unsigned long			preempt_disable_ip;
--- kernel/events/core.c
+++ kernel/events/core.c
@@ -11049,6 +11049,8 @@ int perf_event_init_task(struct task_struct *child)
 	memset(child->perf_event_ctxp, 0, sizeof(child->perf_event_ctxp));
 	mutex_init(&child->perf_event_mutex);
 	INIT_LIST_HEAD(&child->perf_event_list);
+	child->perf_data_user_ptr  = NULL;
+	child->perf_data_user_size = 0;
 
 	for_each_task_context_nr(ctxn) {
 		ret = perf_event_init_context(child, ctxn);
