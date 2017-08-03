81b363da9ae60bc3cf01f33cf1aff3125b1fc7a5
Jiri Olsa <jolsa@kernel.org>
2017-08-03 11:55:30 +0200

    perf record: Enable delayed samples

--- tools/perf/util/evsel.c
+++ tools/perf/util/evsel.c
@@ -50,6 +50,7 @@ static struct {
 	bool lbr_flags;
 	bool write_backward;
 	bool group_read;
+	bool user_data;
 } perf_missing_features;
 
 static clockid_t clockid;
@@ -1054,6 +1055,11 @@ void perf_evsel__config(struct perf_evsel *evsel, struct record_opts *opts,
 	apply_config_terms(evsel, opts);
 
 	evsel->ignore_missing_thread = opts->ignore_missing_thread;
+
+	/* Enable delayed user data processing if there's any. */
+	if ((perf_evsel__is_sample_bit(evsel, CALLCHAIN) && !attr->exclude_callchain_user) ||
+	     perf_evsel__is_sample_bit(evsel, STACK_USER))
+		attr->user_data = true;
 }
 
 static int perf_evsel__alloc_fd(struct perf_evsel *evsel, int ncpus, int nthreads)
@@ -1693,6 +1699,8 @@ fallback_missing_features:
 				     PERF_SAMPLE_BRANCH_NO_CYCLES);
 	if (perf_missing_features.group_read && evsel->attr.inherit)
 		evsel->attr.read_format &= ~(PERF_FORMAT_GROUP|PERF_FORMAT_ID);
+	if (perf_missing_features.user_data)
+		evsel->attr.user_data = false;
 retry_sample_id:
 	if (perf_missing_features.sample_id_all)
 		evsel->attr.sample_id_all = 0;
@@ -1853,6 +1861,10 @@ try_fallback:
 		   (evsel->attr.read_format & PERF_FORMAT_GROUP)) {
 		perf_missing_features.group_read = true;
 		pr_debug2("switching off group read\n");
+		goto fallback_missing_features;
+	} else if (!perf_missing_features.user_data) {
+		perf_missing_features.user_data = true;
+		pr_debug2("switching off user data events\n");
 		goto fallback_missing_features;
 	}
 out_close:
