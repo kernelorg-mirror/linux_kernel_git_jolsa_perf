2b2ab8e7934be147dcc63616bf5cbfd99de953df
Jiri Olsa <jolsa@kernel.org>
2017-08-10 15:49:14 +0200

    perf: Add PERF_SAMPLE_STACK_USER to user data event
    Link: http://lkml.kernel.org/n/tip-viombsvi545yxxy2l8uyz3p9@git.kernel.org
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- include/uapi/linux/perf_event.h
+++ include/uapi/linux/perf_event.h
@@ -940,11 +940,15 @@ enum perf_event_type {
 	 *	#
 	 *	#   PERF_SAMPLE_USER_DATA_ID
 	 *	#   PERF_SAMPLE_CALLCHAIN
+	 *	#   PERF_SAMPLE_STACK_USER
 	 *	#
 	 *	# and governs the data portion:
 	 *
 	 *	{ u64		nr,
 	 *	  u64		ips[nr];}      && PERF_SAMPLE_CALLCHAIN
+	 *	{ u64		size;
+	 * 	  char		data[size];
+	 * 	  u64		dyn_size;}     && PERF_SAMPLE_STACK_USER
 	 *	{ u64		user_data_id;} && PERF_SAMPLE_USER_DATA_ID
 	 *
 	 *	struct sample_id		sample_id;
