cf0513eb69bfa3b0971b04068f55bd2df188d409
Jiri Olsa <jolsa@kernel.org>
2017-10-30 08:39:15 +0100

    perf: Introduce TIF_PERF_USER_DATA bit

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -923,6 +923,7 @@ struct task_struct {
 	struct perf_event_context	*perf_event_ctxp[perf_nr_task_contexts];
 	struct mutex			perf_event_mutex;
 	struct list_head		perf_event_list;
+	int				perf_user_data;
 #endif
 #ifdef CONFIG_DEBUG_PREEMPT
 	unsigned long			preempt_disable_ip;
