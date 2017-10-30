8a6182dba78834bedcd41f321e4117ea11730fd7
Jiri Olsa <jolsa@kernel.org>
2017-10-30 08:29:40 +0100

    perf: Add PERF_RECORD_USER_DATA event processing
    Adding support to skip user data retrieval from event's
    NMI routine and delay that to time when task is jumping
    back to user space.
    
    The idea is the NMI routine to kick task work any time
    there's need to retrieve user data.
    
    Link: http://lkml.kernel.org/n/tip-j8azyyhfgipc6mn8amfpy8hm@git.kernel.org
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -689,6 +689,18 @@ struct perf_event {
 #endif /* CONFIG_PERF_EVENTS */
 };
 
+enum perf_user_data_state {
+	PERF_USER_DATA_STATE_OFF	= 0,
+	PERF_USER_DATA_STATE_ENABLE	= 1,
+	PERF_USER_DATA_STATE_ON		= 2,
+};
+
+struct perf_user_data {
+	struct callback_head		work;
+	u64				type;
+	enum perf_user_data_state	state;
+};
+
 /**
  * struct perf_event_context - event context structure
  *
@@ -740,6 +752,7 @@ struct perf_event_context {
 #endif
 	void				*task_ctx_data; /* pmu specific data */
 	struct rcu_head			rcu_head;
+	struct perf_user_data		user_data;
 };
 
 /*
@@ -947,6 +960,7 @@ extern void perf_prepare_sample(struct perf_event_header *header,
 				struct perf_sample_data *data,
 				struct perf_event *event,
 				struct pt_regs *regs);
+void perf_prepare_sample_fallback(struct perf_event *event);
 
 extern int perf_event_overflow(struct perf_event *event,
 				 struct perf_sample_data *data,
