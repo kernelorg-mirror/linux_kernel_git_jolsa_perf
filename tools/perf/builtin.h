8d52a9383eb65a96bb84fc8d2812d2e8fb3bbf7f
Jiri Olsa <jolsa@kernel.org>
2018-03-02 16:06:02 +0100

    perf tools: Add bpf command
    Adding perf bpf command to allow to provide some
    fucs over ebpf objects, like compile, disassembly
    and loading, which is comming in following patches.
    
    Link: http://lkml.kernel.org/n/tip-51vi69jgn3nfa00azjlikfck@git.kernel.org
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/builtin.h
+++ tools/perf/builtin.h
@@ -39,6 +39,7 @@ int cmd_inject(int argc, const char **argv);
 int cmd_mem(int argc, const char **argv);
 int cmd_data(int argc, const char **argv);
 int cmd_ftrace(int argc, const char **argv);
+int cmd_bpf(int argc, const char **argv);
 
 int find_scripts(char **scripts_array, char **scripts_path_array);
 #endif
