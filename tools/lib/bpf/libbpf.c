b156a7787b7e0e2142562ff7f51d149a85b9af74
Jiri Olsa <jolsa@kernel.org>
2018-03-15 17:29:32 +0100

    libbpf: Rename RELO_LD64 to RELO_LD64_MAP

--- tools/lib/bpf/libbpf.c
+++ tools/lib/bpf/libbpf.c
@@ -190,12 +190,12 @@ struct bpf_program {
 
 	struct reloc_desc {
 		enum {
-			RELO_LD64,
+			RELO_LD64_MAP,
 			RELO_CALL,
 		} type;
 		int insn_idx;
 		union {
-			int map_idx;
+			int map_idx;	/* RELO_LD64_MAP */
 			int text_off;
 		};
 	} *reloc_desc;
@@ -1112,7 +1112,7 @@ collect_reloc_maps(struct reloc_desc *desc, struct bpf_object *obj,
 		return -LIBBPF_ERRNO__RELOC;
 	}
 
-	desc->type = RELO_LD64;
+	desc->type = RELO_LD64_MAP;
 	desc->map_idx = map_idx;
 	return 0;
 }
@@ -1294,7 +1294,7 @@ bpf_program__relocate(struct bpf_program *prog, struct bpf_object *obj)
 		return 0;
 
 	for (i = 0; i < prog->nr_reloc; i++) {
-		if (prog->reloc_desc[i].type == RELO_LD64) {
+		if (prog->reloc_desc[i].type == RELO_LD64_MAP) {
 			struct bpf_insn *insns = prog->insns;
 			int insn_idx, map_idx;
 
