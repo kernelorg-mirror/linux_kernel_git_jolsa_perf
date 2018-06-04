6be1ce188934811403902f6c81634d07e4cd75be
Jiri Olsa <jolsa@kernel.org>
2018-06-04 15:59:01 +0200

    perf stat: Add cputime metric support
    Adding --top/--top-full options to provide metrics based
    on the cputime PMU events. Simply all the metrics are simple
    ratios of events to STAT_NSECS time to get their % value.
    
    The --top option provides basic subset of cputime metrics:
    
      # perf stat --top -I 1000
      #           time       Idle     System       User        Irq    Softirq    IO wait
           1.001692690     100.0%       0.0%       0.0%       0.7%       0.2%       0.0%
           2.002994039      98.9%       0.0%       0.0%       0.9%       0.2%       0.0%
           3.004164038      98.5%       0.2%       0.2%       0.9%       0.2%       0.0%
           4.005312773      98.9%       0.0%       0.0%       0.9%       0.2%       0.0%
    
    The --top-full option provides all cputime metrics:
    
      # perf stat --top-full -I 1000
      #           time       Idle     System       User        Irq    Softirq    IO wait      Guest Guest nice       Nice      Steal
           1.001750803     100.0%       0.0%       0.0%       0.7%       0.2%       0.0%       0.0%       0.0%       0.0%       0.0%
           2.003159490      99.0%       0.0%       0.0%       0.9%       0.2%       0.0%       0.0%       0.0%       0.0%       0.0%
           3.004358366      99.0%       0.0%       0.0%       0.9%       0.2%       0.0%       0.0%       0.0%       0.0%       0.0%
           4.005592436      98.9%       0.0%       0.0%       0.9%       0.2%       0.0%       0.0%       0.0%       0.0%       0.0%
    
    Link: http://lkml.kernel.org/n/tip-zue4s78pxc1cybb954t52ks4@git.kernel.org
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/builtin-stat.c
+++ tools/perf/builtin-stat.c
@@ -133,6 +133,34 @@ static const char *smi_cost_attrs = {
 	"}"
 };
 
+static const char *top_attrs = {
+	"{"
+	"cpu-clock,"
+	"cputime/idle/,"
+	"cputime/system/,"
+	"cputime/user/,"
+	"cputime/irq/,"
+	"cputime/softirq/,"
+	"cputime/iowait/"
+	"}"
+};
+
+static const char *top_full_attrs = {
+	"{"
+	"cpu-clock,"
+	"cputime/idle/,"
+	"cputime/system/,"
+	"cputime/user/,"
+	"cputime/irq/,"
+	"cputime/softirq/,"
+	"cputime/iowait/,"
+	"cputime/guest/,"
+	"cputime/guest_nice/,"
+	"cputime/nice/,"
+	"cputime/steal/"
+	"}"
+};
+
 static struct target target = {
 	.uid	= UINT_MAX,
 };
@@ -143,6 +171,8 @@ static volatile pid_t		child_pid			= -1;
 static int			detailed_run			=  0;
 static bool			transaction_run;
 static bool			topdown_run			= false;
+static bool			top_run				= false;
+static bool			top_run_full			= false;
 static bool			smi_cost			= false;
 static bool			smi_reset			= false;
 static int			big_num_opt			=  -1;
@@ -652,6 +682,8 @@ static const struct option stat_options[] = {
 			"measure topdown level 1 statistics"),
 	OPT_BOOLEAN(0, "smi-cost", &smi_cost,
 			"measure SMI cost"),
+	OPT_BOOLEAN(0, "top", &top_run, "show CPU utilization"),
+	OPT_BOOLEAN(0, "top-full", &top_run_full, "show extended CPU utilization"),
 	OPT_CALLBACK('M', "metrics", &stat_record.evlist, "metric/metric group list",
 		     "monitor specified metrics or metric groups (separated by ,)",
 		     parse_metric_groups),
@@ -1052,6 +1084,21 @@ static int add_default_attributes(void)
 			parse_events_print_error(&errinfo, transaction_attrs);
 			return -1;
 		}
+		return 0;
+	}
+
+	if (top_run || top_run_full) {
+		const char *attrs = top_run ? top_attrs : top_full_attrs;
+
+		err = parse_events(stat_record.evlist, attrs, &errinfo);
+		if (err) {
+			fprintf(stderr, "Cannot set up cputime events\n");
+			parse_events_print_error(&errinfo, attrs);
+			return -1;
+		}
+		if (!force_metric_only)
+			stat_record.config.metric_only = true;
+		stat_record.config.metric_only_len = 10;
 		return 0;
 	}
 
