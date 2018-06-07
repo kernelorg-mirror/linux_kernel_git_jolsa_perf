e4f9c49be3fdcc816bfe9ae90ba4c052940df1d4
Jiri Olsa <jolsa@kernel.org>
2018-06-07 18:54:52 +0200

    perf stat: Move top attrs into stat.c

--- tools/perf/builtin-stat.c
+++ tools/perf/builtin-stat.c
@@ -135,34 +135,6 @@ static const char *smi_cost_attrs = {
 	"}"
 };
 
-static const char *top_attrs = {
-	"{"
-	"cpu-clock,"
-	"cputime/idle/,"
-	"cputime/system/,"
-	"cputime/user/,"
-	"cputime/irq/,"
-	"cputime/softirq/,"
-	"cputime/iowait/"
-	"}"
-};
-
-static const char *top_full_attrs = {
-	"{"
-	"cpu-clock,"
-	"cputime/idle/,"
-	"cputime/system/,"
-	"cputime/user/,"
-	"cputime/irq/,"
-	"cputime/softirq/,"
-	"cputime/iowait/,"
-	"cputime/guest/,"
-	"cputime/guest_nice/,"
-	"cputime/nice/,"
-	"cputime/steal/"
-	"}"
-};
-
 static struct perf_evlist	*evsel_list;
 
 static struct rblist		 metric_events;
