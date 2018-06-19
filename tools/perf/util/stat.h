d829143f4a8a77f8c73a683d8e38c938d28da842
Jiri Olsa <jolsa@kernel.org>
2018-06-19 16:41:57 +0200

    perf stat: Move null_run into struct perf_stat_config

--- tools/perf/util/stat.h
+++ tools/perf/util/stat.h
@@ -113,6 +113,7 @@ struct perf_stat_config {
 	int			 run_count;
 	unsigned int		 metric_only_len;
 	struct stats		*walltime_nsecs_stats;
+	bool			 null_run;
 };
 
 void update_stats(struct stats *stats, u64 val);
