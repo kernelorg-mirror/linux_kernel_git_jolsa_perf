c340dcdea7f30311bcbda0266de84e3c050840cc
Jiri Olsa <jolsa@kernel.org>
2018-06-25 23:37:44 +0200

    perf stat: Add perf_stat_record__read function

--- tools/perf/util/stat.h
+++ tools/perf/util/stat.h
@@ -226,6 +226,8 @@ size_t perf_event__fprintf_stat_config(union perf_event *event, FILE *fp);
 
 int perf_stat_record__open(struct perf_stat_record *record,
 			   struct target *target, bool store_id);
+int perf_stat_record__read(struct perf_stat_record *record,
+			   struct target *target, bool process);
 int perf_stat_synthesize_config(struct perf_stat_config *config,
 				struct perf_tool *tool,
 				struct perf_evlist *evlist,
