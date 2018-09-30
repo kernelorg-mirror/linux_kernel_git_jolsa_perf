a0f08786d1253d89f8d75e849443e670209c8ae5
Jiri Olsa <jolsa@kernel.org>
2018-09-30 22:07:34 +0200

    bpf: Make bpf_get_prog_name public
    So it can be used outside core.c.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -826,5 +826,6 @@ extern const struct bpf_func_proto bpf_get_local_storage_proto;
 /* Shared helpers among cBPF and eBPF. */
 void bpf_user_rnd_init_once(void);
 u64 bpf_user_rnd_u32(u64 r1, u64 r2, u64 r3, u64 r4, u64 r5);
+void bpf_get_prog_name(const struct bpf_prog *prog, char *sym);
 
 #endif /* _LINUX_BPF_H */
