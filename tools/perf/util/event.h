261ff8ee563f26f7224446cc236ab764c633199b
Jiri Olsa <jolsa@kernel.org>
2018-10-29 15:21:12 +0100

    perf tools: Introduce PERF_RECORD_DATA_SORTED event
    Mark sorted data with new PERF_RECORD_DATA_SORTED event,
    which is noow stored via perf inject command.
    
    Once processed on the report side, we switch off events
    sorting and save some cycles
    
      $ perf record -g ./perf bench sched messaging -l 100000
      $ perf inject -i perf.data -o perf-sorted.data
    
      $ perf stat -e instructions:u,cycles:u perf --no-pager report -i perf-sorted.data --stdio --no-children > /dev/null
      ...
       Performance counter stats for './perf --no-pager report -i perf-sorted.data --stdio --no-children':
    
         183,532,276,350      instructions:u            #    1.49  insn per cycle
         122,977,305,665      cycles:u
    
            34.715783688 seconds time elapsed
    
            34.388010000 seconds user
             0.250043000 seconds sys
    
      $ perf stat -e instructions:u,cycles:u perf --no-pager report -i perf.data --stdio --no-children > /dev/null
      ...
    
       Performance counter stats for './perf --no-pager report -i perf.data --stdio --no-children':
    
         183,974,355,298      instructions:u            #    1.48  insn per cycle
         124,219,591,899      cycles:u
    
            35.858622089 seconds time elapsed
    
            35.515006000 seconds user
             0.261993000 seconds sys
    
    Link: http://lkml.kernel.org/n/tip-zg3vz7kjr86cco7lo91v8yhj@git.kernel.org
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/util/event.h
+++ tools/perf/util/event.h
@@ -249,6 +249,7 @@ enum perf_user_event_type { /* above any possible kernel type */
 	PERF_RECORD_EVENT_UPDATE		= 78,
 	PERF_RECORD_TIME_CONV			= 79,
 	PERF_RECORD_HEADER_FEATURE		= 80,
+	PERF_RECORD_DATA_SORTED			= 81,
 	PERF_RECORD_HEADER_MAX
 };
 
