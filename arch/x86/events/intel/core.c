1d6d890b7d9d599da5856274e016c9f946b85648
Jiri Olsa <jolsa@kernel.org>
2018-11-12 21:03:06 +0100

    perf/x86: Add generic branch tracing check to intel_pmu_has_bts
    It's not enought to check only for PERF_COUNT_HW_BRANCH_INSTRUCTIONS
    which is counter of the PERF_TYPE_HARDWARE type. We can have branch
    tracing event defined via PERF_TYPE_RAW type.
    
    Changing the intel_pmu_has_bts code to check on generic hw config
    value, so both HW types are covered.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- arch/x86/events/intel/core.c
+++ arch/x86/events/intel/core.c
@@ -2470,16 +2470,7 @@ done:
 static struct event_constraint *
 intel_bts_constraints(struct perf_event *event)
 {
-	struct hw_perf_event *hwc = &event->hw;
-	unsigned int hw_event, bts_event;
-
-	if (event->attr.freq)
-		return NULL;
-
-	hw_event = hwc->config & INTEL_ARCH_EVENT_MASK;
-	bts_event = x86_pmu.event_map(PERF_COUNT_HW_BRANCH_INSTRUCTIONS);
-
-	if (unlikely(hw_event == bts_event && hwc->sample_period == 1))
+	if (unlikely(intel_pmu_has_bts(event)))
 		return &bts_constraint;
 
 	return NULL;
