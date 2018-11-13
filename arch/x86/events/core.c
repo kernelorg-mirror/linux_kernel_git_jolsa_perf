11dd003fa3dec0c9f21491157001aabe49c79c43
Jiri Olsa <jolsa@kernel.org>
2018-11-13 18:09:22 +0100

    perf/x86: Disallow precise_ip on BTS event
    Do not allow branch tracing for precise events.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- arch/x86/events/core.c
+++ arch/x86/events/core.c
@@ -457,6 +457,10 @@ check_branch:
 		if (!attr->exclude_kernel)
 			return -EOPNOTSUPP;
 
+		/* BTS is not allowed for precise events. */
+		if (attr->precise_ip)
+			return -EOPNOTSUPP;
+
 		/* disallow bts if conflicting events are present */
 		if (x86_add_exclusive(x86_lbr_exclusive_lbr))
 			return -EBUSY;
