b8d505a624b686d43674cfb8d4af87d4fdbfbfd5
Andi Kleen <ak@linux.intel.com>
2018-11-19 21:06:17 -0800

    perf script: Fix LBR skid dump problems in brstackinsn
    This is a fix for another instance of the skid problem Milian
    recently found [1]
    
    The LBRs don't freeze at the exact same time as the PMI is triggered.
    The perf script brstackinsn code that dumps LBR assembler
    assumes that the last branch in the LBR leads to the sample point.
    But with skid it's possible that the CPU executes one or more branches
    before the sample, but which do not appear in the LBR.
    
    What happens then is either that the sample point is before
    the last LBR branch. In this case the dumper sees a negative
    length and ignores it. Or it the sample point is long after
    the last branch. Then the dumper sees a very long block and dumps
    it upto its block limit (16k bytes), which is noise in the output.
    
    On typical sample session this can happen regularly.
    
    This patch tries to detect and handle the situation. On the last
    block that is dumped by the LBR dumper we always stop on the first
    branch. If the block length is negative just scan forward to the
    first branch. Otherwise scan until a branch is found.
    
    The PT decoder already has a function that uses the instruction
    decoder to detect branches, so we can just reuse it here.
    
    Then when a terminating branch is found print an indication
    and stop dumping. This might miss a few instructions, but at least
    shows no runaway blocks.
    
    Cc: milian.wolff@kdab.com
    Cc: adrian.hunter@intel.com
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- tools/perf/builtin-script.c
+++ tools/perf/builtin-script.c
@@ -1049,9 +1049,18 @@ static int perf_sample__fprintf_brstackinsn(struct perf_sample *sample,
 
 	/*
 	 * Print final block upto sample
+	 *
+	 * Due to pipeline delays the LBRs might be missing a branch
+	 * or two, which can result in very large or negative blocks
+	 * between final branch and sample. When this happens just
+	 * continue walking after the last TO until we hit a branch.
 	 */
 	start = br->entries[0].to;
 	end = sample->ip;
+	if (end < start) {
+		/* Missing jump. Scan 128 bytes for the next branch */
+		end = start + 128;
+	}
 	len = grab_bb(buffer, start, end, machine, thread, &x.is64bit, &x.cpumode, true);
 	printed += ip__fprintf_sym(start, thread, x.cpumode, x.cpu, &lastsym, attr, fp);
 	if (len <= 0) {
@@ -1060,7 +1069,6 @@ static int perf_sample__fprintf_brstackinsn(struct perf_sample *sample,
 			      machine, thread, &x.is64bit, &x.cpumode, false);
 		if (len <= 0)
 			goto out;
-
 		printed += fprintf(fp, "\t%016" PRIx64 "\t%s\n", sample->ip,
 			dump_insn(&x, sample->ip, buffer, len, NULL));
 		goto out;
@@ -1070,6 +1078,14 @@ static int perf_sample__fprintf_brstackinsn(struct perf_sample *sample,
 				   dump_insn(&x, start + off, buffer + off, len - off, &ilen));
 		if (ilen == 0)
 			break;
+		if (arch_is_branch(buffer + off, len - off, x.is64bit) &&
+			start + off != sample->ip) {
+			/*
+			 * Hit a missing branch. Just stop.
+			 */
+			printed += fprintf(fp, "\t... not reaching sample ...\n");
+			break;
+		}
 	}
 out:
 	return printed;
