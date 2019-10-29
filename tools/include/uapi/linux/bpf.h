4372ecec7431a378f18b946c620a2c2dc77979cf
Jiri Olsa <jolsa@kernel.org>
2019-10-29 16:51:37 +0100

    bpf: Add d_path helper
    Adding d_path helper function that returns full path for
    given 'struct path' object, which needs to be the kernel
    BTF 'path' object. The path is returned in buffer provided
    'buf' of size 'sz' and is zero terminated.
    
      bpf_d_path(&file->f_path, buf, size);
    
    The helper calls directly d_path function, so there's only
    limited set of function it can be called from. Adding just
    very modest set for the start.
    
    Updating also bpf.h tools uapi header and adding 'path' to
    bpf_helpers_doc.py script.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/include/uapi/linux/bpf.h
+++ tools/include/uapi/linux/bpf.h
@@ -3389,6 +3389,18 @@ union bpf_attr {
  *		A non-negative value equal to or less than *size* on success,
  *		or a negative error in case of failure.
  *
+ * int bpf_d_path(struct path *path, char *buf, u32 sz)
+ *	Description
+ *		Return full path for given 'struct path' object, which
+ *		needs to be the kernel BTF 'path' object. The path is
+ *		returned in buffer provided 'buf' of size 'sz' and
+ *		is zero terminated.
+ *
+ *	Return
+ *		On success, the strictly positive length of the string,
+ *		including the trailing NUL character. On error, a negative
+ *		value.
+ *
  */
 #define __BPF_FUNC_MAPPER(FN)		\
 	FN(unspec),			\
@@ -3533,6 +3545,7 @@ union bpf_attr {
 	FN(skc_to_tcp_request_sock),	\
 	FN(skc_to_udp6_sock),		\
 	FN(get_task_stack),		\
+	FN(d_path),			\
 	/* */
 
 /* integer value in 'imm' field of BPF_CALL instruction selects which helper
