1692989f5ea6485a52df62b1073d4ad96a9df4db
Jiri Olsa <jolsa@kernel.org>
2019-12-02 12:09:30 +0100

    selftests, bpftool: Add build test for libbpf dynamic linking
    Adding new test to test_bpftool_build.sh script to test the
    dynamic linkage of libbpf for bpftool:
    
      $ ./test_bpftool_build.sh
      [SNIP]
    
      ... with dynamic libbpf
    
      $PWD:    /home/jolsa/kernel/linux-perf/tools/bpf/bpftool
      command: make -s -C ../../lib/bpf clean >/dev/null
      command: make -s -C ../../lib/bpf prefix=/tmp/tmp.VgJC3OzEDz install_lib install_headers >/dev/null
      command: make -s clean >/dev/null
      command: make -s LIBBPF_DYNAMIC=1 LIBBPF_DIR=/tmp/tmp.VgJC3OzEDz >/dev/null
      binary:  /home/jolsa/kernel/linux-perf/tools/bpf/bpftool/bpftool
      binary:  linked with libbpf
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/testing/selftests/bpf/test_bpftool_build.sh
+++ tools/testing/selftests/bpf/test_bpftool_build.sh
@@ -85,6 +85,41 @@ make_with_tmpdir() {
 	echo
 }
 
+make_with_dynamic_libbpf() {
+	TMPDIR=$(mktemp -d)
+	echo -e "\$PWD:    $PWD"
+	echo -e "command: make -s -C ../../lib/bpf clean >/dev/null"
+	make $J -s -C ../../lib/bpf prefix=$TMPDIR >/dev/null
+	if [ $? -ne 0 ] ; then
+		ERROR=1
+	fi
+	echo -e "command: make -s -C ../../lib/bpf prefix=$TMPDIR install_lib install_headers >/dev/null"
+	make $J -s -C ../../lib/bpf prefix=$TMPDIR >/dev/null
+	if [ $? -ne 0 ] ; then
+		ERROR=1
+	fi
+	echo -e "command: make -s clean >/dev/null"
+	if [ $? -ne 0 ] ; then
+		ERROR=1
+	fi
+	echo -e "command: make -s LIBBPF_DYNAMIC=1 LIBBPF_DIR=$TMPDIR >/dev/null"
+	make $J -s LIBBPF_DYNAMIC=1 LIBBPF_DIR=$TMPDIR >/dev/null
+	if [ $? -ne 0 ] ; then
+		ERROR=1
+	fi
+	check .
+	ldd bpftool | grep -q libbpf.so
+	if [ $? -ne 0 ] ; then
+		printf "FAILURE: Did not find libbpf linked\n"
+	else
+		echo "binary:  linked with libbpf"
+	fi
+	make -s -C ../../lib/bpf clean
+	make -s clean
+	rm -rf -- $TMPDIR
+	echo
+}
+
 echo "Trying to build bpftool"
 echo -e "... through kbuild\n"
 
@@ -145,3 +180,7 @@ make_and_clean
 make_with_tmpdir OUTPUT
 
 make_with_tmpdir O
+
+echo -e "... with dynamic libbpf\n"
+
+make_with_dynamic_libbpf
