cfe66ab9c53e31adb2e12b3ded4458935f59f485
Jiri Olsa <jolsa@kernel.org>
2020-07-06 23:00:54 +0200

    perf metric: Add add_metric function
    Decouple metric adding logging into add_metric function,
    so it can be used from other places in following changes.
    
    Acked-by: Ian Rogers <irogers@google.com>
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/util/metricgroup.c
+++ tools/perf/util/metricgroup.c
@@ -634,6 +634,39 @@ static struct pmu_event *find_metric(const char *metric, struct pmu_events_map *
 	return NULL;
 }
 
+static int add_metric(struct list_head *group_list,
+		      struct pmu_event *pe,
+		      bool metric_no_group)
+{
+	int ret = 0;
+
+	pr_debug("metric expr %s for %s\n", pe->metric_expr, pe->metric_name);
+
+	if (!strstr(pe->metric_expr, "?")) {
+		ret = __metricgroup__add_metric(group_list,
+						pe,
+						metric_no_group,
+						1);
+	} else {
+		int j, count;
+
+		count = arch_get_runtimeparam();
+
+		/* This loop is added to create multiple
+		 * events depend on count value and add
+		 * those events to group_list.
+		 */
+
+		for (j = 0; j < count && !ret; j++) {
+			ret = __metricgroup__add_metric(
+				group_list, pe,
+				metric_no_group, j);
+		}
+	}
+
+	return ret;
+}
+
 static int metricgroup__add_metric(const char *metric, bool metric_no_group,
 				   struct strbuf *events,
 				   struct list_head *group_list,
@@ -647,33 +680,10 @@ static int metricgroup__add_metric(const char *metric, bool metric_no_group,
 	if (!pe)
 		return -EINVAL;
 
-	pr_debug("metric expr %s for %s\n", pe->metric_expr, pe->metric_name);
+	ret = add_metric(group_list, pe, metric_no_group);
+	if (ret)
+		return ret;
 
-	if (!strstr(pe->metric_expr, "?")) {
-		ret = __metricgroup__add_metric(group_list,
-						pe,
-						metric_no_group,
-						1);
-		if (ret)
-			return ret;
-	} else {
-		int j, count;
-
-		count = arch_get_runtimeparam();
-
-		/* This loop is added to create multiple
-		 * events depend on count value and add
-		 * those events to group_list.
-		 */
-
-		for (j = 0; j < count; j++) {
-			ret = __metricgroup__add_metric(
-				group_list, pe,
-				metric_no_group, j);
-			if (ret)
-				return ret;
-		}
-	}
 	list_for_each_entry(eg, group_list, nd) {
 		if (events->len > 0)
 			strbuf_addf(events, ",");
