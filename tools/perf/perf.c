70a8d9661ab525d1b5444d1ee55a68ce76f4d828
Jiri Olsa <jolsa@kernel.org>
2020-07-17 23:33:57 +0200

    switch off kmem

--- tools/perf/perf.c
+++ tools/perf/perf.c
@@ -62,7 +62,6 @@ static struct cmd_struct commands[] = {
 #ifdef HAVE_LIBELF_SUPPORT
 	{ "probe",	cmd_probe,	0 },
 #endif
-	{ "kmem",	cmd_kmem,	0 },
 	{ "test",	cmd_test,	0 },
 #if defined(HAVE_LIBAUDIT_SUPPORT) || defined(HAVE_SYSCALL_TABLE_SUPPORT)
 	{ "trace",	cmd_trace,	0 },
