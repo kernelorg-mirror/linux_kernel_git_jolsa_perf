0b485f2eec4a582e9e3fae74419f88e7359e38eb
Jiri Olsa <jolsa@kernel.org>
2020-07-17 23:34:19 +0200

    switch off inject

--- tools/perf/perf.c
+++ tools/perf/perf.c
@@ -66,7 +66,6 @@ static struct cmd_struct commands[] = {
 #if defined(HAVE_LIBAUDIT_SUPPORT) || defined(HAVE_SYSCALL_TABLE_SUPPORT)
 	{ "trace",	cmd_trace,	0 },
 #endif
-	{ "inject",	cmd_inject,	0 },
 	{ "data",	cmd_data,	0 },
 };
 
