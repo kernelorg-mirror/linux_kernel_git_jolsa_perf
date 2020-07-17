2861b00395c40aaa7fe907d6d331c15aa3eb2619
Jiri Olsa <jolsa@kernel.org>
2020-07-17 23:22:05 +0200

    switch off config

--- tools/perf/perf.c
+++ tools/perf/perf.c
@@ -57,7 +57,6 @@ struct cmd_struct {
 static struct cmd_struct commands[] = {
 	{ "buildid-cache", cmd_buildid_cache, 0 },
 	{ "buildid-list", cmd_buildid_list, 0 },
-	{ "config",	cmd_config,	0 },
 	{ "c2c",	cmd_c2c,	0 },
 	{ "diff",	cmd_diff,	0 },
 	{ "evlist",	cmd_evlist,	0 },
