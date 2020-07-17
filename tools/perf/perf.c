0fb7d6912b7039bf238a31a55f7ba0cff7adc48f
Jiri Olsa <jolsa@kernel.org>
2020-07-17 23:34:44 +0200

    switch off data

--- tools/perf/perf.c
+++ tools/perf/perf.c
@@ -66,7 +66,6 @@ static struct cmd_struct commands[] = {
 #if defined(HAVE_LIBAUDIT_SUPPORT) || defined(HAVE_SYSCALL_TABLE_SUPPORT)
 	{ "trace",	cmd_trace,	0 },
 #endif
-	{ "data",	cmd_data,	0 },
 };
 
 struct pager_config {
