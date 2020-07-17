b45c97272e67d95437231559538fc4a9e57cd73c
Jiri Olsa <jolsa@kernel.org>
2020-07-17 23:29:20 +0200

    switch off list

--- tools/perf/perf.c
+++ tools/perf/perf.c
@@ -56,7 +56,6 @@ struct cmd_struct {
 
 static struct cmd_struct commands[] = {
 	{ "c2c",	cmd_c2c,	0 },
-	{ "list",	cmd_list,	0 },
 	{ "record",	cmd_record,	0 },
 	{ "report",	cmd_report,	0 },
 	{ "stat",	cmd_stat,	0 },
