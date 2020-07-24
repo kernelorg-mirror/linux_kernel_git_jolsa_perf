b89a31252b694f73bb1a2b3808adcc2bdc3eb7fa
Wang ShaoBo <bobo.shaobowang@huawei.com>
2020-07-24 18:07:06 +0800

    perf jvmti: Check return value of fwrite_unlocked in jvmti_agent.c
    Function jvmti_write_code called by compiled_method_load_cb may return
    error in using fwrite_unlocked, this failure should be captured and
    warned.
    
    Signed-off-by: Wang ShaoBo <bobo.shaobowang@huawei.com>
    Cc: Cheng Jian <cj.chengjian@huawei.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Li Bin <huawei.libin@huawei.com>
    Link: http://lore.kernel.org/lkml/20200724100706.48330-1-bobo.shaobowang@huawei.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/jvmti/jvmti_agent.c
+++ tools/perf/jvmti/jvmti_agent.c
@@ -363,7 +363,7 @@ jvmti_write_code(void *agent, char const *sym,
 	struct jr_code_load rec;
 	size_t sym_len;
 	FILE *fp = agent;
-	int ret = -1;
+	int ret;
 
 	/* don't care about 0 length function, no samples */
 	if (size == 0)
@@ -401,16 +401,23 @@ jvmti_write_code(void *agent, char const *sym,
 	rec.code_index = code_generation++;
 
 	ret = fwrite_unlocked(&rec, sizeof(rec), 1, fp);
-	fwrite_unlocked(sym, sym_len, 1, fp);
+	if (ret)
+		goto out_error;
+	ret = fwrite_unlocked(sym, sym_len, 1, fp);
+	if (ret)
+		goto out_error;
 
-	if (code)
-		fwrite_unlocked(code, size, 1, fp);
+	if (code) {
+		ret = fwrite_unlocked(code, size, 1, fp);
+		if (ret)
+			goto out_error;
+	}
 
 	funlockfile(fp);
-
-	ret = 0;
-
-	return ret;
+	return 0;
+out_error:
+	funlockfile(fp);
+	return -1;
 }
 
 int
