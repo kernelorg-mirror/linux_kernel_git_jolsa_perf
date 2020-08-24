daa2cedc63c92f1a4803b2f4b3e9e349f3a97b2e
Jiri Olsa <jolsa@kernel.org>
2020-08-24 22:42:00 +0200

    perf tools: Call test_attr__open directly
    There's no longer need to have test_attr__open inside
    sys_perf_event_open call, because both record and stat
    call evsel__open_cpu, so we can call it directly from
    there and not polute perf-sys.h header.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/util/evsel.c
+++ tools/perf/util/evsel.c
@@ -1680,6 +1680,9 @@ retry_open:
 
 			FD(evsel, cpu, thread) = fd;
 
+			if (unlikely(test_attr__enabled))
+				test_attr__open(&evsel->core.attr, pid, cpus->map[cpu], fd, group_fd, flags);
+
 			if (fd < 0) {
 				err = -errno;
 
