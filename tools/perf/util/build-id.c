a08055d9859c89e7ec3e3fa7b69876771e556129
Jiri Olsa <jolsa@kernel.org>
2020-09-08 12:42:49 +0200

    perf tools: Use machine__for_each_dso in perf_session__cache_build_ids

--- tools/perf/util/build-id.c
+++ tools/perf/util/build-id.c
@@ -856,11 +856,22 @@ out_free:
 	return err;
 }
 
-static int dso__cache_build_id(struct dso *dso, struct machine *machine)
+struct cache_build_ids_data {
+	bool with_hits;
+};
+
+static int dso__cache_build_id(struct dso *dso, struct machine *machine, void *priv)
 {
+	struct cache_build_ids_data *data = priv;
 	bool is_kallsyms = dso__is_kallsyms(dso);
 	bool is_vdso = dso__is_vdso(dso);
 	const char *name = dso->long_name;
+
+	if (!dso->has_build_id)
+		return 0;
+
+	if (data->with_hits && !dso->hit)
+		return 0;
 
 	if (dso__is_kcore(dso)) {
 		is_kallsyms = true;
@@ -870,32 +881,25 @@ static int dso__cache_build_id(struct dso *dso, struct machine *machine)
 				     dso->nsinfo, is_kallsyms, is_vdso);
 }
 
-static int __dsos__cache_build_ids(struct list_head *head,
-				   struct machine *machine,
-				   bool with_hits)
+static int
+machines__for_each_dso(struct machines *machines, machine__dso_t fn, void *priv)
 {
-	struct dso *pos;
-	int err = 0;
+	int ret = machine__for_each_dso(&machines->host, fn, priv);
+	struct rb_node *nd;
 
-	dsos__for_each_with_build_id(pos, head) {
-		if (with_hits && !pos->hit)
-			continue;
-		if (dso__cache_build_id(pos, machine))
-			err = -1;
+	for (nd = rb_first_cached(&machines->guests); nd;
+	     nd = rb_next(nd)) {
+		struct machine *pos = rb_entry(nd, struct machine, rb_node);
+		ret |= machine__for_each_dso(pos, fn, priv);
 	}
-
-	return err;
-}
-
-static int machine__cache_build_ids(struct machine *machine, bool with_hits)
-{
-	return __dsos__cache_build_ids(&machine->dsos.head, machine, with_hits);
+	return ret ? -1 : 0;
 }
 
 int perf_session__cache_build_ids(struct perf_session *session, bool with_hits)
 {
-	struct rb_node *nd;
-	int ret;
+	struct cache_build_ids_data data = {
+		.with_hits = with_hits,
+	};
 
 	if (no_buildid_cache)
 		return 0;
@@ -903,14 +907,7 @@ int perf_session__cache_build_ids(struct perf_session *session, bool with_hits)
 	if (mkdir(buildid_dir, 0755) != 0 && errno != EEXIST)
 		return -1;
 
-	ret = machine__cache_build_ids(&session->machines.host, with_hits);
-
-	for (nd = rb_first_cached(&session->machines.guests); nd;
-	     nd = rb_next(nd)) {
-		struct machine *pos = rb_entry(nd, struct machine, rb_node);
-		ret |= machine__cache_build_ids(pos, with_hits);
-	}
-	return ret ? -1 : 0;
+	return machines__for_each_dso(&session->machines, dso__cache_build_id, &data) ?  -1 : 0;
 }
 
 static bool machine__read_build_ids(struct machine *machine, bool with_hits)
