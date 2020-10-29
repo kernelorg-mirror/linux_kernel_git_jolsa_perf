4c704bce795d69a9db9d2d21d11612bc78b3e54a
Steven Rostedt (VMware) <rostedt@goodmis.org>
2020-10-28 20:08:19 -0400

    livepatching: Use the default ftrace_ops instead of REGS when ARGS is available
    When CONFIG_HAVE_DYNAMIC_FTRACE_WITH_ARGS is available, the ftrace call
    will be able to set the ip of the calling function. This will improve the
    performance of live kernel patching where it does not need all the regs to
    be stored just to change the instruction pointer.
    
    If all archs that support live kernel patching also support
    HAVE_DYNAMIC_FTRACE_WITH_ARGS, then the architecture specific function
    klp_arch_set_pc() could be made generic.
    
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- arch/x86/include/asm/ftrace.h
+++ arch/x86/include/asm/ftrace.h
@@ -62,6 +62,9 @@ arch_ftrace_get_regs(struct ftrace_regs *fregs)
 		return NULL;
 	return &fregs->regs;
 }
+
+#define ftrace_regs_set_ip(fregs, _ip)		\
+	do { (fregs)->regs.ip = (_ip); } while (0)
 #endif
 
 #endif /*  CONFIG_DYNAMIC_FTRACE */
