11222a15904d02a4aa6c801b5293310f7bdd2d81
Jiri Olsa <jolsa@kernel.org>
2020-12-11 18:18:50 +0100

    perf daemon: Allow only one daemon over base directory
    Add 'lock' file under daemon base and flock it, so only one
    perf daemon can run on top of it.
    
      # perf daemon --config ./config.daemon
      # perf daemon --config ./config.daemon
      failed: another perf daemon (pid 369675) owns /opt/perfdata
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/builtin-daemon.c
+++ tools/perf/builtin-daemon.c
@@ -8,6 +8,7 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/file.h>
 #include <signal.h>
 #include <stdlib.h>
 #include <time.h>
@@ -514,12 +515,18 @@ static int cmd_session_list(struct daemon *daemon, union cmd *cmd, FILE *out)
 			/* output */
 			csv_sep, daemon->base, SESSION_OUTPUT);
 
+		fprintf(out, "%c%s/%s",
+			/* lock */
+			csv_sep, daemon->base, "lock");
+
 		fprintf(out, "\n");
 	} else {
 		fprintf(out, "[%d:daemon] base: %s\n", getpid(), daemon->base);
 		if (cmd->list.verbose) {
 			fprintf(out, "  output:  %s/%s\n",
 				SESSION_OUTPUT, daemon->base);
+			fprintf(out, "  lock:    %s/lock\n",
+				daemon->base);
 		}
 	}
 
@@ -713,6 +720,42 @@ static int handle_config_changes(struct daemon *daemon, int conf_fd,
 	return 0;
 }
 
+static int check_lock(struct daemon *daemon)
+{
+	char path[PATH_MAX];
+	char buf[20];
+	int fd, pid;
+	ssize_t len;
+
+	scnprintf(path, sizeof(path), "%s/lock", daemon->base);
+
+	fd = open(path, O_RDWR|O_CREAT, 0640);
+	if (fd < 0)
+		return -1;
+
+	if (lockf(fd, F_TLOCK, 0) < 0) {
+		filename__read_int(path, &pid);
+		fprintf(stderr, "failed: another perf daemon (pid %d) owns %s\n",
+			pid, daemon->base);
+		return -1;
+	}
+
+	scnprintf(buf, sizeof(buf), "%d", getpid());
+	len = strlen(buf);
+
+	if (write(fd, buf, len) != len) {
+		perror("write failed");
+		return -1;
+	}
+
+	if (ftruncate(fd, len)) {
+		perror("ftruncate failed");
+		return -1;
+	}
+
+	return 0;
+}
+
 static int go_background(struct daemon *daemon)
 {
 	int pid, fd;
@@ -725,6 +768,9 @@ static int go_background(struct daemon *daemon)
 		return 1;
 
 	if (setsid() < 0)
+		return -1;
+
+	if (check_lock(daemon))
 		return -1;
 
 	umask(0);
@@ -793,6 +839,9 @@ static int __cmd_start(struct daemon *daemon, struct option parent_options[],
 		usage_with_options(daemon_usage, start_options);
 
 	if (setup_server_config(daemon))
+		return -1;
+
+	if (foreground && check_lock(daemon))
 		return -1;
 
 	if (!foreground && go_background(daemon))
