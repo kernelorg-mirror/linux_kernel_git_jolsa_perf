7291e99e1f7607cae36f8cab81685bd6a0d3971f
Jiri Olsa <jolsa@kernel.org>
2020-12-15 00:21:00 +0100

    perf daemon: Add up time for daemon/session list
    Display up time for both daemon and sessions, like:
    
      # perf daemon -v
      [690174:daemon] base: /opt/perfdata
        output:  /opt/perfdata/output
        lock:    /opt/perfdata/lock
        up:      87 minutes
      [690175:top] perf record -e cycles --switch-output=1m --switch-max-files=6 -a
        base:    /opt/perfdata/top
        output:  /opt/perfdata/top/output
        control: /opt/perfdata/top/control
        ack:     /opt/perfdata/top/ack
        up:      20 minutes
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/builtin-daemon.c
+++ tools/perf/builtin-daemon.c
@@ -24,6 +24,7 @@
 #include <fcntl.h>
 #include <sys/inotify.h>
 #include <libgen.h>
+#include <time.h>
 #include "builtin.h"
 #include "perf.h"
 #include "debug.h"
@@ -49,6 +50,7 @@ struct session {
 	int			 pid;
 	struct list_head	 list;
 	enum session_state	 state;
+	time_t			 start;
 };
 
 struct daemon {
@@ -59,6 +61,7 @@ struct daemon {
 	char			*base;
 	struct list_head	 sessions;
 	FILE			*out;
+	time_t			 start;
 };
 
 static struct daemon __daemon = {
@@ -295,6 +298,8 @@ static int session__run(struct session *session, struct daemon *daemon)
 		return -1;
 	}
 
+	session->start = time(NULL);
+
 	session->pid = fork();
 	if (session->pid < 0)
 		return -1;
@@ -519,6 +524,7 @@ static int cmd_session_list(struct daemon *daemon, union cmd *cmd, FILE *out)
 {
 	char csv_sep = cmd->list.csv_sep;
 	struct session *session;
+	time_t curr = time(NULL);
 
 	if (csv_sep) {
 		fprintf(out, "%d%c%s%c%s%c%s/%s",
@@ -533,6 +539,10 @@ static int cmd_session_list(struct daemon *daemon, union cmd *cmd, FILE *out)
 			/* lock */
 			csv_sep, daemon->base, "lock");
 
+		fprintf(out, "%c%lu",
+			/* session up time */
+			csv_sep, (curr - daemon->start) / 60);
+
 		fprintf(out, "\n");
 	} else {
 		fprintf(out, "[%d:daemon] base: %s\n", getpid(), daemon->base);
@@ -541,6 +551,8 @@ static int cmd_session_list(struct daemon *daemon, union cmd *cmd, FILE *out)
 				SESSION_OUTPUT, daemon->base);
 			fprintf(out, "  lock:    %s/lock\n",
 				daemon->base);
+			fprintf(out, "  up:      %lu minutes\n",
+				(curr - daemon->start) / 60);
 		}
 	}
 
@@ -566,6 +578,10 @@ static int cmd_session_list(struct daemon *daemon, union cmd *cmd, FILE *out)
 				/* session ack */
 				csv_sep, daemon->base, session->name, SESSION_ACK);
 
+			fprintf(out, "%c%lu",
+				/* session up time */
+				csv_sep, (curr - session->start) / 60);
+
 			fprintf(out, "\n");
 		} else {
 			fprintf(out, "[%d:%s] perf record %s\n",
@@ -580,6 +596,8 @@ static int cmd_session_list(struct daemon *daemon, union cmd *cmd, FILE *out)
 				daemon->base, session->name);
 			fprintf(out, "  ack:     %s/%s/" SESSION_ACK "\n",
 				daemon->base, session->name);
+			fprintf(out, "  up:      %lu minutes\n",
+				(curr - session->start) / 60);
 		}
 	}
 
@@ -861,6 +879,8 @@ static int __cmd_start(struct daemon *daemon, struct option parent_options[],
 	argc = parse_options(argc, argv, start_options, daemon_usage, 0);
 	if (argc)
 		usage_with_options(daemon_usage, start_options);
+
+	daemon->start = time(NULL);
 
 	if (setup_server_config(daemon))
 		return -1;
