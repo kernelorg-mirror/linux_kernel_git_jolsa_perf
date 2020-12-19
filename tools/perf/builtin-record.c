91f73b90b90e88aa2138d54bd5747e0a17278ce0
Jiri Olsa <jolsa@kernel.org>
2020-12-20 00:24:24 +0100

    perf tools: Add stop cotnrol command
    Adding control 'stop' command to stop perf record.
    When it is received, perf will set done = 1.
    
    Example session:
    
      terminal 1:
        # mkfifo control ack
        # perf record --control=fifo:control,ack
    
      terminal 2:
        # echo stop > control
    
      terminal 1:
        ...
        [ perf record: Woken up 7 times to write data ]
        [ perf record: Captured and wrote 3.214 MB perf.data (38280 samples) ]
        #
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/builtin-record.c
+++ tools/perf/builtin-record.c
@@ -1941,6 +1941,9 @@ static int __cmd_record(struct record *rec, int argc, const char **argv)
 				hit_auxtrace_snapshot_trigger(rec);
 				evlist__ctlfd_ack(rec->evlist);
 				break;
+			case EVLIST_CTL_CMD_STOP:
+				done = 1;
+				break;
 			case EVLIST_CTL_CMD_ACK:
 			case EVLIST_CTL_CMD_UNSUPPORTED:
 			case EVLIST_CTL_CMD_ENABLE:
