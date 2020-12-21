26133384486eba8fa05ff0ca1139bf11388c73fc
Jiri Olsa <jolsa@kernel.org>
2020-12-21 18:14:35 +0100

    perf daemon: Add signal command
    Allow perf daemon to send SIGUSR2 to all running sessions:
    
      # perf daemon
      [1:364758] perf record -m 10M -e cycles -o /opt/perfdata/1/perf.data --overwrite --switch-output -a
      [2:364759] perf record -m 10M -e sched:* -o /opt/perfdata/2/perf.data --overwrite --switch-output -a
    
      # perf daemon -s
      signal 12 sent to session '1 [92187]'
      signal 12 sent to session '2 [92188]'
    
    Or to specific one:
    
      # perf daemon --signal=1
      signal 12 sent to session '1 [364758]'
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/builtin-daemon.c
+++ tools/perf/builtin-daemon.c
@@ -475,8 +475,11 @@ static int setup_server_socket(struct daemon *daemon)
 
 enum {
 	CMD_LIST = 0,
+	CMD_SIGNAL = 1,
 	CMD_MAX,
 };
+
+#define SESSION_MAX 64
 
 union cmd {
 	int cmd;
@@ -487,6 +490,13 @@ union cmd {
 		int	verbose;
 		char	csv_sep;
 	} list;
+
+	/* CMD_LIST */
+	struct {
+		int	cmd;
+		int	sig;
+		char	name[SESSION_MAX];
+	} signal;
 };
 
 static int cmd_session_list(struct daemon *daemon, union cmd *cmd, FILE *out)
@@ -544,6 +554,24 @@ static int cmd_session_list(struct daemon *daemon, union cmd *cmd, FILE *out)
 	return 0;
 }
 
+static int cmd_session_kill(struct daemon *daemon, union cmd *cmd, FILE *out)
+{
+	struct session *session;
+	bool all = false;
+
+	all = !strcmp(cmd->signal.name, "all");
+
+	list_for_each_entry(session, &daemon->sessions, list) {
+		if (all || !strcmp(cmd->signal.name, session->name)) {
+			session__signal(session, cmd->signal.sig);
+			fprintf(out, "signal %d sent to session '%s [%d]'\n",
+				cmd->signal.sig, session->name, session->pid);
+		}
+	}
+
+	return 0;
+}
+
 static int handle_server_socket(struct daemon *daemon, int sock_fd)
 {
 	int ret = -EINVAL, fd;
@@ -570,6 +598,9 @@ static int handle_server_socket(struct daemon *daemon, int sock_fd)
 	switch (cmd.cmd) {
 	case CMD_LIST:
 		ret = cmd_session_list(daemon, &cmd, out);
+		break;
+	case CMD_SIGNAL:
+		ret = cmd_session_kill(daemon, &cmd, out);
 		break;
 	default:
 		break;
@@ -864,6 +895,36 @@ static int send_cmd_list(struct daemon *daemon)
 	return send_cmd(daemon, &cmd);
 }
 
+static int __cmd_signal(struct daemon *daemon, struct option parent_options[],
+			int argc, const char **argv)
+{
+	const char *sigstr = NULL;
+	const char *name = "all";
+	struct option start_options[] = {
+		OPT_STRING(0, "session", &name, "session",
+			"Sent signal to specific session"),
+		OPT_STRING(0, "session", &sigstr, "signal",
+			"Send specific signal"),
+		OPT_PARENT(parent_options),
+		OPT_END()
+	};
+	int sig = SIGUSR2;
+	union cmd cmd;
+
+	argc = parse_options(argc, argv, start_options, daemon_usage, 0);
+	if (argc)
+		usage_with_options(daemon_usage, start_options);
+
+	if (sigstr)
+		sig = atoi(sigstr);
+
+	cmd.signal.cmd = CMD_SIGNAL,
+	cmd.signal.sig = sig;
+	strncpy(cmd.signal.name, name, sizeof(cmd.signal.name) - 1);
+
+	return send_cmd(daemon, &cmd);
+}
+
 int cmd_daemon(int argc, const char **argv)
 {
 	struct option daemon_options[] = {
@@ -889,8 +950,12 @@ int cmd_daemon(int argc, const char **argv)
 		return __cmd_start(&__daemon, daemon_options, argc, argv);
 
 	if (argc) {
-		pr_err("failed: unknown command '%s'\n", argv[0]);
-		return -1;
+		if (!strcmp(argv[0], "signal")) {
+			return __cmd_signal(&__daemon, daemon_options, argc, argv);
+		} else {
+			pr_err("failed: unknown command '%s'\n", argv[0]);
+			return -1;
+		}
 	}
 
 	return send_cmd_list(&__daemon);
