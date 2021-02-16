bab2d650c08189d2e98d2e7da9bf09707cb82685
Jiri Olsa <jolsa@kernel.org>
2021-02-16 22:40:40 +0100

    perf: Add PERF_FORMAT_BUILD_ID_FAULTS

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -1858,6 +1858,9 @@ static void __perf_event_read_size(struct perf_event *event, int nr_siblings)
 	if (event->attr.read_format & PERF_FORMAT_ID)
 		entry += sizeof(u64);
 
+	if (event->attr.read_format & PERF_FORMAT_BUILD_ID_FAULTS)
+		entry += sizeof(u64);
+
 	if (event->attr.read_format & PERF_FORMAT_GROUP) {
 		nr += nr_siblings;
 		size += sizeof(u64);
@@ -5143,11 +5146,15 @@ static int __perf_read_group_add(struct perf_event *leader,
 	values[n++] += perf_event_count(leader);
 	if (read_format & PERF_FORMAT_ID)
 		values[n++] = primary_event_id(leader);
+	if (read_format & PERF_FORMAT_BUILD_ID_FAULTS)
+		values[n++] = local64_read(&leader->build_id_faults);
 
 	for_each_sibling_event(sub, leader) {
 		values[n++] += perf_event_count(sub);
 		if (read_format & PERF_FORMAT_ID)
 			values[n++] = primary_event_id(sub);
+		if (read_format & PERF_FORMAT_BUILD_ID_FAULTS)
+			values[n++] = local64_read(&sub->build_id_faults);
 	}
 
 	raw_spin_unlock_irqrestore(&ctx->lock, flags);
@@ -5204,7 +5211,7 @@ static int perf_read_one(struct perf_event *event,
 				 u64 read_format, char __user *buf)
 {
 	u64 enabled, running;
-	u64 values[4];
+	u64 values[5];
 	int n = 0;
 
 	values[n++] = __perf_event_read_value(event, &enabled, &running);
@@ -5214,6 +5221,8 @@ static int perf_read_one(struct perf_event *event,
 		values[n++] = running;
 	if (read_format & PERF_FORMAT_ID)
 		values[n++] = primary_event_id(event);
+	if (read_format & PERF_FORMAT_BUILD_ID_FAULTS)
+		values[n++] = local64_read(&event->build_id_faults);
 
 	if (copy_to_user(buf, values, n * sizeof(u64)))
 		return -EFAULT;
@@ -6689,7 +6698,7 @@ static void perf_output_read_one(struct perf_output_handle *handle,
 				 u64 enabled, u64 running)
 {
 	u64 read_format = event->attr.read_format;
-	u64 values[4];
+	u64 values[5];
 	int n = 0;
 
 	values[n++] = perf_event_count(event);
@@ -6703,6 +6712,8 @@ static void perf_output_read_one(struct perf_output_handle *handle,
 	}
 	if (read_format & PERF_FORMAT_ID)
 		values[n++] = primary_event_id(event);
+	if (read_format & PERF_FORMAT_BUILD_ID_FAULTS)
+		values[n++] = local64_read(&event->build_id_faults);
 
 	__output_copy(handle, values, n * sizeof(u64));
 }
@@ -6713,7 +6724,7 @@ static void perf_output_read_group(struct perf_output_handle *handle,
 {
 	struct perf_event *leader = event->group_leader, *sub;
 	u64 read_format = event->attr.read_format;
-	u64 values[5];
+	u64 values[6];
 	int n = 0;
 
 	values[n++] = 1 + leader->nr_siblings;
@@ -6731,6 +6742,8 @@ static void perf_output_read_group(struct perf_output_handle *handle,
 	values[n++] = perf_event_count(leader);
 	if (read_format & PERF_FORMAT_ID)
 		values[n++] = primary_event_id(leader);
+	if (read_format & PERF_FORMAT_BUILD_ID_FAULTS)
+		values[n++] = local64_read(&leader->build_id_faults);
 
 	__output_copy(handle, values, n * sizeof(u64));
 
@@ -6744,6 +6757,8 @@ static void perf_output_read_group(struct perf_output_handle *handle,
 		values[n++] = perf_event_count(sub);
 		if (read_format & PERF_FORMAT_ID)
 			values[n++] = primary_event_id(sub);
+		if (read_format & PERF_FORMAT_BUILD_ID_FAULTS)
+			values[n++] = local64_read(&sub->build_id_faults);
 
 		__output_copy(handle, values, n * sizeof(u64));
 	}
