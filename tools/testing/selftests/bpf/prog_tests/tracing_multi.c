56db5b6a7a9ccb318e4f23c5d49bc8247200bd5c
Jiri Olsa <jolsa@kernel.org>
2021-05-24 23:58:50 +0200

    selftests/bpf: Add fentry tracing multi func test
    Adding selftest for fentry multi func test that attaches
    to bpf_fentry_test* functions and checks argument values
    based on the processed function.
    
    We need to cast to real arguments types in multi_arg_check,
    because the checked value can be shorter than u64.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

---
+++ tools/testing/selftests/bpf/prog_tests/tracing_multi.c
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <test_progs.h>
+#include "tracing_multi_fentry_test.skel.h"
+#include "trace_helpers.h"
+
+static void multi_fentry_test(void)
+{
+	LIBBPF_OPTS(bpf_test_run_opts, topts);
+	struct tracing_multi_fentry_test *skel = NULL;
+	int err, prog_fd;
+
+	skel = tracing_multi_fentry_test__open_and_load();
+	if (!ASSERT_OK_PTR(skel, "fentry_multi_skel_load"))
+		goto cleanup;
+
+	err = tracing_multi_fentry_test__attach(skel);
+	if (!ASSERT_OK(err, "fentry_attach"))
+		goto cleanup;
+
+	prog_fd = bpf_program__fd(skel->progs.test);
+	err = bpf_prog_test_run_opts(prog_fd, &topts);
+	ASSERT_OK(err, "test_run");
+
+	ASSERT_EQ(skel->bss->test_result, 8, "test_result");
+
+cleanup:
+	tracing_multi_fentry_test__destroy(skel);
+}
+
+void test_tracing_multi_test(void)
+{
+	if (test__start_subtest("fentry"))
+		multi_fentry_test();
+}
