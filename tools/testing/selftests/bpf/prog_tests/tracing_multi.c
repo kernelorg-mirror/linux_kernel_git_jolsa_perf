44150e3c73f7a93df1b1f990bedba9ca7c946674
Jiri Olsa <jolsa@kernel.org>
2021-05-25 22:37:47 +0200

    selftests/bpf: Add fentry/fexit tracing multi func test
    Adding selftest for fentry/fexit multi func tests that attaches
    to bpf_fentry_test* functions and checks argument values based
    on the processed function.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/testing/selftests/bpf/prog_tests/tracing_multi.c
+++ tools/testing/selftests/bpf/prog_tests/tracing_multi.c
@@ -2,6 +2,7 @@
 #include <test_progs.h>
 #include "tracing_multi_fentry_test.skel.h"
 #include "tracing_multi_fexit_test.skel.h"
+#include "tracing_multi_fentry_fexit_test.skel.h"
 #include "trace_helpers.h"
 
 static void multi_fentry_test(void)
@@ -53,10 +54,38 @@ cleanup:
 	tracing_multi_fexit_test__destroy(skel);
 }
 
+static void multi_fentry_fexit_test(void)
+{
+	LIBBPF_OPTS(bpf_test_run_opts, topts);
+	struct tracing_multi_fentry_fexit_test *skel = NULL;
+	int err, prog_fd;
+
+	skel = tracing_multi_fentry_fexit_test__open_and_load();
+	if (!ASSERT_OK_PTR(skel, "tracing_multi_fentry_fexit_test__open_and_load"))
+		goto cleanup;
+
+	err = tracing_multi_fentry_fexit_test__attach(skel);
+	if (!ASSERT_OK(err, "tracing_multi_fentry_fexit_test__attach"))
+		goto cleanup;
+
+	prog_fd = bpf_program__fd(skel->progs.test2);
+	err = bpf_prog_test_run_opts(prog_fd, &topts);
+	ASSERT_OK(err, "test_run");
+
+	ASSERT_EQ(skel->bss->test1_arg_result, 8, "test1_arg_result");
+	ASSERT_EQ(skel->bss->test2_arg_result, 8, "test2_arg_result");
+	ASSERT_EQ(skel->bss->test2_ret_result, 8, "test2_ret_result");
+
+cleanup:
+	tracing_multi_fentry_fexit_test__destroy(skel);
+}
+
 void test_tracing_multi_test(void)
 {
 	if (test__start_subtest("fentry"))
 		multi_fentry_test();
 	if (test__start_subtest("fexit"))
 		multi_fexit_test();
+	if (test__start_subtest("fentry_fexit"))
+		multi_fentry_fexit_test();
 }
