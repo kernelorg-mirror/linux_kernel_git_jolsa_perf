75c50c5a148820bf4b1fafa3fb834bbfaeccc6c2
Jiri Olsa <jolsa@kernel.org>
2021-09-04 17:44:13 +0200

    bpf: Add support for tracing multi link
    Adding new link to allow to attach program to multiple
    function BTF IDs.
    
    New fields are added to bpf_attr::link_create to pass
    array of BTF IDs:
    
      struct {
        __aligned_u64   btf_ids;        /* addresses to attach */
        __u32           btf_ids_cnt;    /* addresses count */
      } multi;
    
    The new link code will load these IDs into bpf_tramp_id
    and resolve their ips.
    
    The resolve itself is done as per Andrii's suggestion:
    
      - lookup all names for given IDs
      - store and sort them by name
      - go through all kallsyms symbols and use bsearch
        to find it in provided names
      - if name is found, store the address for the name
      - resort the names array based on ID
    
    If there are multi symbols of the same name the first one
    will be used to resolve the address.
    
    The new link will pass them to the bpf_tramp_attach that
    does all the work of attaching.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/include/uapi/linux/bpf.h
+++ tools/include/uapi/linux/bpf.h
@@ -1017,6 +1017,7 @@ enum bpf_link_type {
 	BPF_LINK_TYPE_PERF_EVENT = 7,
 	BPF_LINK_TYPE_KPROBE_MULTI = 8,
 	BPF_LINK_TYPE_STRUCT_OPS = 9,
+	BPF_LINK_TYPE_TRACING_MULTI = 10,
 
 	MAX_BPF_LINK_TYPE,
 };
@@ -1503,6 +1504,10 @@ union bpf_attr {
 				 */
 				__u64		cookie;
 			} tracing;
+			struct {
+				__aligned_u64	btf_ids;	/* addresses to attach */
+				__u32		btf_ids_cnt;	/* addresses count */
+			} tracing_multi;
 		};
 	} link_create;
 
