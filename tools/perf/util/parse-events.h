d3cb12aeba50517c27cf68fe58640e128a3aba87
Jiri Olsa <jolsa@kernel.org>
2021-10-29 17:26:35 +0200

    perf tools: Add evsel__new_tp callback to parse_state_ops
    Adding evsel__new_tp callback to parse_state_ops to allow
    custom tracepoint event allocation for event parsing.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/util/parse-events.h
+++ tools/perf/util/parse-events.h
@@ -132,6 +132,7 @@ struct parse_events_error {
 
 struct parse_events_ops {
 	struct evsel* (*evsel__new)(struct perf_event_attr *attr, int idx);
+	struct evsel* (*evsel__new_tp)(const char *sys, const char *name, int idx);
 };
 
 struct parse_events_state {
