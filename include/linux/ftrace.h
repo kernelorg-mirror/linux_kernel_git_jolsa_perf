70eff3e2fd66aeb9ad534d5d0a867ed69192ab8e
Jiri Olsa <jolsa@kernel.org>
2022-06-16 00:28:50 +0200

    ftrace: Add set_ftrace_direct function

--- include/linux/ftrace.h
+++ include/linux/ftrace.h
@@ -367,6 +367,7 @@ extern int ftrace_direct_func_count;
 int register_ftrace_direct(unsigned long ip, unsigned long addr);
 int unregister_ftrace_direct(unsigned long ip, unsigned long addr);
 int modify_ftrace_direct(unsigned long ip, unsigned long old_addr, unsigned long new_addr);
+int set_ftrace_direct(struct ftrace_hash *set);
 int ftrace_modify_direct_caller(struct ftrace_func_entry *entry,
 				struct dyn_ftrace *rec,
 				unsigned long old_addr,
