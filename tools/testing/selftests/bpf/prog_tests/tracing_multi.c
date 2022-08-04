6bd9e83aaf333576ebf26870668c0ffe5c018c6a
Jiri Olsa <jolsa@kernel.org>
2022-08-04 09:55:41 +0200

    selftests/bpf: Add fexit tracing multi func test
    Adding selftest for fexit multi func test that attaches
    to bpf_fentry_test* functions and checks argument values
    based on the processed function.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/testing/selftests/bpf/prog_tests/tracing_multi.c
+++ tools/testing/selftests/bpf/prog_tests/tracing_multi.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <test_progs.h>
 #include "tracing_multi_fentry_test.skel.h"
+#include "tracing_multi_fexit_test.skel.h"
 #include "trace_helpers.h"
 
 static void multi_fentry_test(void)
@@ -27,8 +28,35 @@ cleanup:
 	tracing_multi_fentry_test__destroy(skel);
 }
 
+static void multi_fexit_test(void)
+{
+	LIBBPF_OPTS(bpf_test_run_opts, topts);
+	struct tracing_multi_fexit_test *skel = NULL;
+	int err, prog_fd;
+
+	skel = tracing_multi_fexit_test__open_and_load();
+	if (!ASSERT_OK_PTR(skel, "tracing_multi_fentry_test__open_and_load"))
+		goto cleanup;
+
+	err = tracing_multi_fexit_test__attach(skel);
+	if (!ASSERT_OK(err, "tracing_multi_fentry_test__attach"))
+		goto cleanup;
+
+	prog_fd = bpf_program__fd(skel->progs.test);
+	err = bpf_prog_test_run_opts(prog_fd, &topts);
+	ASSERT_OK(err, "test_run");
+
+	ASSERT_EQ(skel->bss->test_arg_result, 8, "test_arg_result");
+	ASSERT_EQ(skel->bss->test_ret_result, 8, "test_ret_result");
+
+cleanup:
+	tracing_multi_fexit_test__destroy(skel);
+}
+
 void test_tracing_multi_test(void)
 {
 	if (test__start_subtest("fentry"))
 		multi_fentry_test();
+	if (test__start_subtest("fexit"))
+		multi_fexit_test();
 }
