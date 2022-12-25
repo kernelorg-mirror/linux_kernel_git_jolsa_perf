3a65bfd13e724dc56d81aafb0d8f0a3b4313eea3
Jiri Olsa <jolsa@kernel.org>
2022-12-26 00:53:57 +0100

    selftests/bpf: Add fentry split tracing multi func test

---
+++ tools/testing/selftests/bpf/progs/tracing_multi_fentry_split.c
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+
+char _license[] SEC("license") = "GPL";
+
+__u64 test1_result = 0;
+__u64 test2_result = 0;
+
+__hidden extern void multi_arg_check(__u64 *ctx, __u64 *test_result);
+
+SEC("fentry.multi")
+int BPF_PROG(prog1)
+{
+	multi_arg_check(ctx, &test1_result);
+	return 0;
+}
+
+SEC("fentry.multi")
+int BPF_PROG(prog2)
+{
+	multi_arg_check(ctx, &test2_result);
+	return 0;
+}
