ff6655a44578afd0d00b14077a96b3708449dfa1
Jiri Olsa <jolsa@kernel.org>
2023-06-12 23:56:34 +0200

    bpf: Force kprobe multi expected_attach_type for kprobe_multi link
    We currently allow to create perf link for program with
    expected_attach_type == BPF_TRACE_KPROBE_MULTI.
    
    This will cause crash when we call helpers like get_attach_cookie or
    get_func_ip in such program, because it will call the kprobe_multi's
    version (current->bpf_ctx context setup) of those helpers while it
    expects perf_link's current->bpf_ctx context setup.
    
    Making sure that we use BPF_TRACE_KPROBE_MULTI expected_attach_type
    only for programs attaching through kprobe_multi link.
    
    Fixes: ca74823c6e16 ("bpf: Add cookie support to programs attached with kprobe multi link")
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -4689,6 +4689,11 @@ static int link_create(union bpf_attr *attr, bpfptr_t uattr)
 		ret = bpf_perf_link_attach(attr, prog);
 		break;
 	case BPF_PROG_TYPE_KPROBE:
+		if (prog->expected_attach_type == BPF_TRACE_KPROBE_MULTI &&
+		    attr->link_create.attach_type != BPF_TRACE_KPROBE_MULTI) {
+			ret = -EINVAL;
+			goto out;
+		}
 		if (attr->link_create.attach_type == BPF_PERF_EVENT)
 			ret = bpf_perf_link_attach(attr, prog);
 		else
