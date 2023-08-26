e3d0e2f52a3ad83fa3e9706300460c600c4831de
Jiri Olsa <jolsa@kernel.org>
2023-08-27 00:21:46 +0200

    bpf: Add link_info support for uprobe multi link
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/include/uapi/linux/bpf.h
+++ tools/include/uapi/linux/bpf.h
@@ -6526,6 +6526,15 @@ struct bpf_link_info {
 			__u64 missed;
 		} kprobe_multi;
 		struct {
+			__aligned_u64 path;
+			__aligned_u64 offsets;
+			__aligned_u64 ref_ctr_offsets;
+			__u32 path_max; /* in/out: uprobe_multi path size */
+			__u32 count;    /* in/out: uprobe_multi offsets/ref_ctr_offsets count */
+			__u32 flags;
+			__u32 pid;
+		} uprobe_multi;
+		struct {
 			__u32 type; /* enum bpf_perf_event_type */
 			__u32 :32;
 			union {
