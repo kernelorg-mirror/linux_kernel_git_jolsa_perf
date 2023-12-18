67e3b0c60f9c3f6cbcd97ce5a651012adb9fdd67
Jiri Olsa <jolsa@kernel.org>
2023-12-18 12:12:18 +0100

    bpf: Rename kprobe_multi_link_handler function
    Renaming kprobe_multi_link_handler function to
    kprobe_multi_link_entry_handler to make clear it's
    entry probe handler.
    
    It will ease up following changes.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -2806,9 +2806,9 @@ kprobe_multi_link_prog_run(struct bpf_kprobe_multi_link *link,
 }
 
 static int
-kprobe_multi_link_handler(struct fprobe *fp, unsigned long fentry_ip,
-			  unsigned long ret_ip, struct pt_regs *regs,
-			  void *data)
+kprobe_multi_link_entry_handler(struct fprobe *fp, unsigned long fentry_ip,
+				unsigned long ret_ip, struct pt_regs *regs,
+				void *data)
 {
 	struct bpf_kprobe_multi_link *link;
 
@@ -3042,7 +3042,7 @@ int bpf_kprobe_multi_link_attach(const union bpf_attr *attr, struct bpf_prog *pr
 	if (flags & BPF_F_KPROBE_MULTI_RETURN)
 		link->fp.exit_handler = kprobe_multi_link_exit_handler;
 	else
-		link->fp.entry_handler = kprobe_multi_link_handler;
+		link->fp.entry_handler = kprobe_multi_link_entry_handler;
 
 	link->addrs = addrs;
 	link->cookies = cookies;
