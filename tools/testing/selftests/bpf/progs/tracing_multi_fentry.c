48132820c5a21b82d1a4507a86a67b3742fb110e
Jiri Olsa <jolsa@kernel.org>
2024-10-20 18:03:02 +0200

    debug

--- tools/testing/selftests/bpf/progs/tracing_multi_fentry.c
+++ tools/testing/selftests/bpf/progs/tracing_multi_fentry.c
@@ -9,7 +9,7 @@ __u64 test_result = 0;
 
 __hidden extern void multi_arg_check(__u64 *ctx, __u64 *test_result);
 
-SEC("fentry.multi/bpf_fentry_test*")
+SEC("fentry.multi/xfs*")
 int BPF_PROG(test, __u64 a, __u64 b, __u64 c, __u64 d, __u64 e, __u64 f)
 {
 	multi_arg_check(ctx, &test_result);
