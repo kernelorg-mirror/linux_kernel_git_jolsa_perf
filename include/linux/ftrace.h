d3b3651746bfbd33574554d40ff09752db140b32
Jiri Olsa <jolsa@kernel.org>
2024-11-12 23:41:12 +0100

    ftrace: Add modify_ftrace_direct_hash function

--- include/linux/ftrace.h
+++ include/linux/ftrace.h
@@ -426,6 +426,7 @@ int modify_ftrace_direct_nolock(struct ftrace_ops *ops, unsigned long addr);
 
 int register_ftrace_direct_hash(struct ftrace_ops *ops, struct ftrace_hash *hash);
 int unregister_ftrace_direct_hash(struct ftrace_ops *ops, struct ftrace_hash *hash);
+int modify_ftrace_direct_hash(struct ftrace_ops *ops, struct ftrace_hash *hash);
 
 void ftrace_stub_direct_tramp(void);
 
@@ -459,6 +460,11 @@ int register_ftrace_direct_hash(struct ftrace_ops *ops, struct ftrace_hash *hash
 }
 
 int unregister_ftrace_direct_hash(struct ftrace_ops *ops, struct ftrace_hash *hash)
+{
+	return -ENODEV;
+}
+
+int modify_ftrace_direct_hash(struct ftrace_ops *ops, struct ftrace_hash *hash)
 {
 	return -ENODEV;
 }
