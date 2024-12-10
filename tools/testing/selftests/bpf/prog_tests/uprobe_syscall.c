102971ed2cddf9e734bd1af5002d3c995ed4f0a7
Jiri Olsa <jolsa@kernel.org>
2024-12-11 00:01:24 +0100

    selftests/bpf: Add uprobe syscall sigill signal test
    Make sure that calling uprobe syscall from outside uprobe trampoline
    results in sigill signal.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/testing/selftests/bpf/prog_tests/uprobe_syscall.c
+++ tools/testing/selftests/bpf/prog_tests/uprobe_syscall.c
@@ -232,8 +232,11 @@ __naked unsigned long uretprobe_syscall_call_1(void)
 		"pushq %rax\n"
 		"pushq %rcx\n"
 		"pushq %r11\n"
+		"pushq %rdi\n"
+		"movq $1, %rdi\n"
 		"movq $" __stringify(__NR_uprobe) ", %rax\n"
 		"syscall\n"
+		"popq %rdi\n"
 		"popq %r11\n"
 		"popq %rcx\n"
 		"retq\n"
@@ -672,6 +675,39 @@ cleanup:
 		pthread_join(threads[i], NULL);
 }
 
+static void test_uprobe_sigill(void)
+{
+	int status, err, pid;
+
+	pid = fork();
+	if (!ASSERT_GE(pid, 0, "fork"))
+		return;
+	/* child */
+	if (pid == 0) {
+		asm volatile (
+			"pushq %rax\n"
+			"pushq %rcx\n"
+			"pushq %r11\n"
+			"pushq %rdi\n"
+			"movq $0, %rdi\n"
+			"movq $" __stringify(__NR_uprobe) ", %rax\n"
+			"syscall\n"
+			"popq %rdi\n"
+			"popq %r11\n"
+			"popq %rcx\n"
+			"retq\n"
+		);
+		exit(0);
+	}
+
+	err = waitpid(pid, &status, 0);
+	ASSERT_EQ(err, pid, "waitpid");
+
+	/* verify the child got killed with SIGILL */
+	ASSERT_EQ(WIFSIGNALED(status), 1, "WIFSIGNALED");
+	ASSERT_EQ(WTERMSIG(status), SIGILL, "WTERMSIG");
+}
+
 static void __test_uprobe_syscall(void)
 {
 	if (test__start_subtest("uretprobe_regs_equal"))
@@ -692,6 +728,8 @@ static void __test_uprobe_syscall(void)
 		test_uprobe_usdt();
 	if (test__start_subtest("uprobe_race"))
 		test_uprobe_race();
+	if (test__start_subtest("uprobe_sigill"))
+		test_uprobe_sigill();
 }
 #else
 static void __test_uprobe_syscall(void)
