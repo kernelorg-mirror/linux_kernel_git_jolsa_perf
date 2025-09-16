1e8d7f94dede60c93ac0b7968677f84c725bf1fc
Jiri Olsa <jolsa@kernel.org>
2025-09-16 23:52:56 +0200

    bpf: Allow uprobe program to change context registers
    [ Upstream commit 7384893d970ea114952aef54ad7e3d7d2ca82d4f ]
    
    Currently uprobe (BPF_PROG_TYPE_KPROBE) program can't write to the
    context registers data. While this makes sense for kprobe attachments,
    for uprobe attachment it might make sense to be able to change user
    space registers to alter application execution.
    
    Since uprobe and kprobe programs share the same type (BPF_PROG_TYPE_KPROBE),
    we can't deny write access to context during the program load. We need
    to check on it during program attachment to see if it's going to be
    kprobe or uprobe.
    
    Storing the program's write attempt to context and checking on it
    during the attachment.
    
    Acked-by: Andrii Nakryiko <andrii@kernel.org>
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    Link: https://lore.kernel.org/r/20250916215301.664963-2-jolsa@kernel.org
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -1197,6 +1197,7 @@ struct bpf_prog_aux {
 	bool sleepable;
 	bool tail_call_reachable;
 	bool xdp_has_frags;
+	bool kprobe_write_ctx;
 	/* BTF_KIND_FUNC_PROTO for valid attach_btf_id */
 	const struct btf_type *attach_func_proto;
 	/* function name for valid attach_btf_id */
