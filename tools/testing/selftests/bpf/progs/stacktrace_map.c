ef961526664c0c3a8778b7375ba888c9b051efdb
Jiri Olsa <jolsa@kernel.org>
2025-10-01 16:20:37 +0200

    selftests/bpf: Add stacktrace test for tracing programs
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/testing/selftests/bpf/progs/stacktrace_map.c
+++ tools/testing/selftests/bpf/progs/stacktrace_map.c
@@ -127,4 +127,18 @@ int kprobe_test(struct pt_regs *ctx)
 	return 0;
 }
 
+SEC("fentry/bpf_fentry_test1")
+int BPF_PROG(fentry)
+{
+	test_stackmap(ctx, TRACING_SKIP);
+	return 0;
+}
+
+SEC("fexit/bpf_fentry_test1")
+int BPF_PROG(fexit)
+{
+	test_stackmap(ctx, TRACING_SKIP);
+	return 0;
+}
+
 char _license[] SEC("license") = "GPL";
